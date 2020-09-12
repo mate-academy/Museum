f4af5414430332632285b28d3a700c14707f7d41
Alisa <aliceblunt8@gmail.com>
2020-09-12 15:03:15 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-undef */
 'use strict';
+
+// Slick slider
+$(document).ready(function() {
+  $('.news__wrapper').slick({
+    dots: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1125,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          arrows: true,
+          dots: true,
+          autoplay: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
