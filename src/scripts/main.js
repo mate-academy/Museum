4f10e7077634ca40d9d77fd35b932fe5a626cd10
Lora Levitska <larakad94@gmail.com>
2020-12-03 18:12:25 +0200

    fix & add menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const burger = document.querySelector('.header__burger');
+const toggler = document.querySelector('.burger__menu');
+
+toggler.onclick = function toggleSidebar() {
+  burger.classList.toggle('header__burger--open');
+};
