a96fadd25b95460f74d2a61a2ec1cc908a2ecb6b
Dima <dimontregyb@email.ua>
2020-12-03 12:00:31 +0200

    finish effect &  adaptiv all sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,9 @@ const burgerLine = document.querySelector('.menu__burger-line');
 const menuNavWrapp = document.querySelector('.menu__nav-wrapper');
 const wrapperBurger = document.querySelector('.menu__wrapper-burger');
 const headerLogo = document.querySelector('.header__logo');
+const menuLogo = document.querySelector('.menu__logo');
 const menuLink = document.querySelector('.menu__list');
-const lockScroll = document.querySelector('body');
+const lockScroll = document.querySelector('.body');
 
 menuBurger.onclick = function() {
   menuBurger.classList.toggle('menu__burger--active');
@@ -14,6 +15,7 @@ menuBurger.onclick = function() {
   menuNavWrapp.classList.toggle('menu__nav-wrapper--active');
   wrapperBurger.classList.toggle('menu__wrapper-burger--active');
   headerLogo.classList.toggle('header__logo--active');
+  menuLogo.classList.toggle('menu__logo--active');
   lockScroll.classList.toggle('body--lock');
 };
 
@@ -23,14 +25,23 @@ menuLink.onclick = function() {
   menuNavWrapp.classList.remove('menu__nav-wrapper--active');
   wrapperBurger.classList.remove('menu__wrapper-burger--active');
   headerLogo.classList.remove('header__logo--active');
+  menuLogo.classList.toggle('menu__logo--active');
   lockScroll.classList.remove('body--lock');
 };
 
-headerLogo.onclick = function() {
+menuLogo.onclick = function() {
   menuBurger.classList.remove('menu__burger--active');
   burgerLine.classList.remove('menu__burger-line--active');
   menuNavWrapp.classList.remove('menu__nav-wrapper--active');
   wrapperBurger.classList.remove('menu__wrapper-burger--active');
   headerLogo.classList.remove('header__logo--active');
+  menuLogo.classList.toggle('menu__logo--active');
   lockScroll.classList.remove('body--lock');
 };
+
+const subscribeForm = document.querySelector('.subscribe__form');
+
+subscribeForm.addEventListener('submit', (event) => {
+  document.location.reload();
+  event.preventDefault();
+});
