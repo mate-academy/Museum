49780b42076eb4b65767214a1241197f349ac871
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-07 13:37:47 +0200

    burger menu was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const nav = document.querySelector('.header__menu');
+const toggler = document.querySelector('.toggler');
+
+toggler.onclick = function toggleSidebar() {
+  nav.classList.toggle('header__menu--has-popup');
+  toggler.classList.toggle('toggler--open');
+};
