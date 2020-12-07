65cb9a99b24e5fd771eca608b4f9f5aa94c1a4e7
vovador <vovadorko@gmail.com>
2020-12-07 11:33:46 +0200

    burger menu was implemented and some small inprovments were done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menu = document.querySelector('.header__menu');
+const burger = document.querySelector('.header__burger');
+const logo = document.querySelector('.header__logo-text');
+const title = document.querySelector('.header__title');
+const link = document.querySelector('.header__link');
+const sideText = document.querySelector('.header__side-text');
+
+burger.onclick = function headerMenu() {
+  menu.classList.toggle('header__menu--open');
+  burger.classList.toggle('header__burger--active');
+  logo.classList.toggle('header__logo-text--positioned');
+  title.classList.toggle('header__title--positioned');
+  link.classList.toggle('header__link--positioned');
+  sideText.classList.toggle('header__side-text--positioned');
+};
