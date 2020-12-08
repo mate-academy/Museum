a02bdfbd657c9b5abe7d62d102f3cc3070d61fc6
vovador <vovadorko@gmail.com>
2020-12-08 12:25:23 +0200

    header logic was changed so that header__title & header__link have constant position relatively to header bgIm what enables making a fragment of title transparent color; as well centering of menu nav was improved on large screens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,13 @@
 const menu = document.querySelector('.header__menu');
 const burger = document.querySelector('.header__burger');
 const logo = document.querySelector('.header__logo-text');
-const title = document.querySelector('.header__title');
-const link = document.querySelector('.header__link');
 const sideText = document.querySelector('.header__side-text');
+const hRight = document.querySelector('.header__right');
 
 burger.onclick = function headerMenu() {
   menu.classList.toggle('header__menu--open');
   burger.classList.toggle('header__burger--active');
   logo.classList.toggle('header__logo-text--positioned');
-  title.classList.toggle('header__title--positioned');
-  link.classList.toggle('header__link--positioned');
   sideText.classList.toggle('header__side-text--positioned');
+  hRight.classList.toggle('header__right--positioned');
 };
