ec4f114be158eccf77b4cde118879eb7516032b2
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-08 10:38:54 +0200

    toggler was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,9 @@
 
 const nav = document.querySelector('.header__menu');
 const toggler = document.querySelector('.toggler');
+const lab = document.querySelector('.header__label-for-toggler');
 
-toggler.onclick = function toggleSidebar() {
+lab.onclick = function toggleSidebar() {
   nav.classList.toggle('header__menu--has-popup');
   toggler.classList.toggle('toggler--open');
 };
