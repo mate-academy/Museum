76b06ed40a2beb8d8bcceb9308b868a1884a8673
chri911 <delightsome@bigmir.net>
2020-12-08 20:31:26 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const burger = document.querySelector('.header__toggler');
+
+burger.onclick = function toggleSidebar() {
+  const toggler = document.querySelector('.toggler');
+  const nav = document.querySelector('.nav');
+
+  nav.classList.toggle('nav--open');
+  toggler.classList.toggle('toggler--open');
+};
