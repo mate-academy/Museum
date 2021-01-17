9c109d3894524b2ae597b88e1101ddfc07f780e5
Lora Levitska <larakad94@gmail.com>
2021-01-17 16:14:45 +0200

    add carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,17 @@ const toggler = document.querySelector('.burger__menu');
 toggler.onclick = function toggleSidebar() {
   burger.classList.toggle('header__burger--open');
 };
+
+const width = 300;
+const list = document.querySelector('.gallery__switch-images');
+let position = 0;
+
+document.querySelector('.prev').onclick = function() {
+  position = position + width > 0 ? -900 : position + width;
+  list.style.marginLeft = position + 'px';
+};
+
+document.querySelector('.next').onclick = function() {
+  position = position - width < -900 ? 0 : position - width;
+  list.style.marginLeft = position + 'px';
+};
