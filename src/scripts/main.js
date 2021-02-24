ca56dc4a0ab5fce3bfc4817a74ac307eef911237
Oleg Hluhk <glug.oleg@gmail.com>
2021-02-24 18:00:26 +0200

    without mobile menu and gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.burger-memu');
+const navigation = document.querySelector('.header-nav');
+const logo = document.querySelector('.header__logo');
+const burgerMenuLine = document.querySelector('.burger-memu__line');
+
+burgerMenu.addEventListener('click', function(event) {
+  navigation.classList.toggle('header-nav--active');
+  logo.classList.toggle('header__logo--disable');
+  burgerMenuLine.classList.toggle('burger-memu__line--active');
+  burgerMenu.classList.toggle('burger-memu--active');
+}
+);
