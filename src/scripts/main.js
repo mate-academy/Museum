f95a593c7f886ef2dc4d2f879b94c33a3b5bd34c
Oleg Hluhk <glug.oleg@gmail.com>
2021-02-25 13:04:08 +0200

    v0.9

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,13 @@ const burgerMenu = document.querySelector('.burger-memu');
 const navigation = document.querySelector('.header-nav');
 const logo = document.querySelector('.header__logo');
 const burgerMenuLine = document.querySelector('.burger-memu__line');
+const headerNavigation = document.querySelector('.header__nav');
 
 burgerMenu.addEventListener('click', function(event) {
   navigation.classList.toggle('header-nav--active');
   logo.classList.toggle('header__logo--disable');
   burgerMenuLine.classList.toggle('burger-memu__line--active');
   burgerMenu.classList.toggle('burger-memu--active');
+  headerNavigation.classList.toggle('header__nav--active');
 }
 );
