d9152ba1d857b382d75c728342226f92dd386d57
Oleg Hluhk <glug.oleg@gmail.com>
2021-02-25 19:10:01 +0200

    fix comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,12 @@ burgerMenu.addEventListener('click', function(event) {
   headerNavigation.classList.toggle('header__nav--active');
 }
 );
+
+navigation.addEventListener('click', function(event) {
+  navigation.classList.remove('header-nav--active');
+  logo.classList.remove('header__logo--disable');
+  burgerMenuLine.classList.remove('burger-memu__line--active');
+  burgerMenu.classList.remove('burger-memu--active');
+  headerNavigation.classList.remove('header__nav--active');
+}
+);
