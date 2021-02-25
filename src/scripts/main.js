0658d87ac2cb7edf4423f437203157a3e63b85b4
Iryna Haiduk <$ git config --global user.name Iryna>
2021-02-25 15:02:52 +0200

    Finished creating landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.js-header__nav');
+const menuOpen = document.querySelector('.js-header__burger');
+const menuClose = document.querySelector('.js-nav__close');
+
+menuOpen.addEventListener('click', function() {
+  menu.style.transform = 'translateY(0)';
+});
+
+menuClose.addEventListener('click', function() {
+  menu.style.transform = 'translateY(-100%)';
+});
