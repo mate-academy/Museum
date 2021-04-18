b6dacb4b65fa551424e2ccf6ff27c46a9d9e530e
Serhii Nikitin <serg.niki@i.ua>
2021-04-18 15:31:20 +0300

    some form fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.querySelector('.subscription__form');
+const input = document.querySelector('.subscription__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  input.value = '';
+});
