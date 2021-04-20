4669fa9be7d8ab1028f8faece7f1fd01faf69721
Ivan Shpynda <ivanshpynda@gmail.com>
2021-04-20 14:00:03 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const hamburger = document.querySelector('.header__hamburger');
+const nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', function() {
+  nav.classList.toggle('nav--active');
+  hamburger.classList.toggle('header__hamburger--close');
+});
