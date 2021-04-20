93bf1a74cbf4f18145b430dea2e5f43cc56a3583
Bohdan Klius <bohdanklius@gmail.com>
2021-04-20 15:32:02 +0300

    fix remark

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const toggler = document.querySelector('.header__menu-toggler');
+const menu = document.querySelector('.menu');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('header__menu-toggler--active');
+  menu.classList.toggle('menu--active');
+});
