423bb2fa4eb212daf15ef60562acd019348bcb73
Dmytro Yurchyk <yurchik.d.work@gmail.com>
2021-06-16 21:45:13 +0300

    5 %

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerLink = document.querySelector('.burger-menu__link');
+const burgerHtml = document.querySelector('html');
+const headerLink = document.querySelector('.header__logo-link');
+
+hamburger.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger-menu--active');
+  hamburger.classList.toggle('hamburger--active');
+});
+
+burgerLink.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger-menu--active');
+  hamburger.classList.toggle('hamburger--active');
+  burgerHtml.classList.toggle('html--active');
+});
+
+headerLink.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger-menu--active');
+  hamburger.classList.toggle('hamburger--active');
+  burgerHtml.classList.toggle('html--active');
+});
+
+hamburger.addEventListener('click', () => {
+  burgerHtml.classList.toggle('html--active');
+});
