9e462966ccd48582cd8ef99d86532c69d70d3166
Dmytro Yurchyk <yurchik.d.work@gmail.com>
2021-06-18 00:54:08 +0300

    10%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.header__hamburger');
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerLink = document.querySelector('.burger-menu__link');
-const burgerHtml = document.querySelector('html');
+const burgerHtml = document.querySelector('.page');
 const headerLink = document.querySelector('.header__logo-link');
 
 hamburger.addEventListener('click', () => {
   burgerMenu.classList.toggle('burger-menu--active');
-  hamburger.classList.toggle('hamburger--active');
+  hamburger.classList.toggle('header__hamburger--active');
 });
 
 burgerLink.addEventListener('click', () => {
   burgerMenu.classList.toggle('burger-menu--active');
-  hamburger.classList.toggle('hamburger--active');
-  burgerHtml.classList.toggle('html--active');
+  hamburger.classList.toggle('header__hamburger--active');
+  burgerHtml.classList.toggle('page--active');
 });
 
 headerLink.addEventListener('click', () => {
-  burgerMenu.classList.toggle('burger-menu--active');
-  hamburger.classList.toggle('hamburger--active');
-  burgerHtml.classList.toggle('html--active');
+  burgerMenu.classList.remove('burger-menu--active');
+  hamburger.classList.remove('header__hamburger--active');
+  burgerHtml.classList.remove('page--active');
 });
 
 hamburger.addEventListener('click', () => {
-  burgerHtml.classList.toggle('html--active');
+  burgerHtml.classList.toggle('page--active');
 });
