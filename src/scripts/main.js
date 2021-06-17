d2a27ba18901ce3a27393ea15568dc737bdf8d50
Sasha Kozlovskyy <alex33507@gmail.com>
2021-06-18 00:13:30 +0300

    First try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+const h = document.querySelector(".icon-menu");
+const n = document.querySelector(".nav");
+
+h.addEventListener("click", () => {
+  console.log('icon clicked');
+  n.classList.toggle("nav--activ");
+});
+
+h.addEventListener()
+
+
+// if(activ) {
+//     n.classList.remove("nav--activ")
+// } else {
+//     n.classList.add("nav--activ")
+// }
+
+// const f = n.classList.contains("nav");
+// console.log(f);
+
+// const activ = n.classList.contains("nav--activ");
+// console.log(activ);
+// if(activ) {
+//     n.classList.remove("nav--activ");
+// } else {
+//     n.classList.add("nav--activ");
+// }
