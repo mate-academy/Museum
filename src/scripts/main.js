354fd65293eaf82d308f81af592436ff60378835
Artem <temaqwerty15@gmail.com>
2021-06-17 22:32:39 +0300

    fin mob

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.footer__forms');
+const inputs = document.querySelectorAll('.footer__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
