5fb16e3ba5dad4e6a4f04739beb13f7732e2cb2d
Yaroslav <gso218695@gmail.com>
2021-07-05 22:37:29 +0300

    last time pls

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-100%';
+  }
+  prevScrollpos = currentScrollPos;
+};
