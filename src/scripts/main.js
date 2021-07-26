73a714fc937942715688b6508c9a6b06323ce06e
Moroz Dmytro <dmytro101100@gmail.com>
2021-07-26 11:19:59 +0300

    JS file was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,25 @@
+'use strict'
+
 $(window).on('load resize orientationchange', function() {
-  $('.gallery__photos').each(function(){
-      var $carousel = $(this);
-      if ($(window).width() > 1024) {
-          if ($carousel.hasClass('slick-initialized')) {
-              $carousel.slick('unslick');
-          }
+  $('.gallery__photos').each(function() {
+    const $carousel = $(this);
+
+    if ($(window).width() > 1024) {
+      if ($carousel.hasClass('slick-initialized')) {
+        $carousel.slick('unslick');
       }
-      else{
-          if (!$carousel.hasClass('slick-initialized')) {
-            $carousel.slick({
-              arrows: false,
-              dots: true,
-              adaptiveHeight: true,
-              infinite: false,
-              variableWidth: true,
-              mobileFirst: true,
-            });
-          }
+    }
+    else {
+      if (!$carousel.hasClass('slick-initialized')) {
+        $carousel.slick({
+          arrows: false,
+          dots: true,
+          adaptiveHeight: true,
+          infinite: false,
+          variableWidth: true,
+          mobileFirst: true,
+        });
       }
+    }
   });
 });
