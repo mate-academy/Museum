d42ab3e09476a4fc37ce5076377b98874344d2ad
Moroz Dmytro <dmytro101100@gmail.com>
2021-07-26 11:19:04 +0300

    Remove js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+$(window).on('load resize orientationchange', function() {
+  $('.gallery__photos').each(function(){
+      var $carousel = $(this);
+      if ($(window).width() > 1024) {
+          if ($carousel.hasClass('slick-initialized')) {
+              $carousel.slick('unslick');
+          }
+      }
+      else{
+          if (!$carousel.hasClass('slick-initialized')) {
+            $carousel.slick({
+              arrows: false,
+              dots: true,
+              adaptiveHeight: true,
+              infinite: false,
+              variableWidth: true,
+              mobileFirst: true,
+            });
+          }
+      }
+  });
+});
