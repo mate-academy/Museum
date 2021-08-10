1699d601b8e7929e3127d28693a07d0e7981e39d
Anastasiia Diachenko <anastasiiadiachenko1@gmail.com>
2021-08-10 17:00:51 +0300

    all parts done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const navToogler = document.querySelector('.icon--menu');
+const navWrapper = document.querySelector('.menu');
+
+navToogler.addEventListener('click', () => {
+  navWrapper.classList.toggle('menu--active');
+});
+
+navToogler.addEventListener();
