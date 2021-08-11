06e3736f2faf446d2e12f066db77b02636d8def6
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-08-11 23:17:57 +0300

    add burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const navigationList = document.querySelector('.header__nav-items');
+const checkbox = document.querySelector('.header__nav-toggler');
+const headerLogo = document.querySelector('.header__logo');
+
+checkbox.addEventListener('click', () => {
+  if (checkbox.checked === true) {
+    navigationList.classList.add('header__nav-items--active');
+    headerLogo.classList.add('header__logo--hiden');
+  }
+
+  if (checkbox.checked === false) {
+    navigationList.classList.remove('header__nav-items--active');
+    headerLogo.classList.remove('header__logo--hiden');
+  }
+});
+
+navigationList.addEventListener('click', () => {
+  checkbox.checked = false;
+  navigationList.classList.remove('header__nav-items--active');
+});
