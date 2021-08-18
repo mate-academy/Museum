8df8ee506e4f10a914af0f07f9e317d2167740ab
Maria Kovach <maria_kovach@ukr.net>
2021-08-18 12:41:41 +0300

    changes applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const btn = document.getElementById('page__top-btn');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
+}
+
+btn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
