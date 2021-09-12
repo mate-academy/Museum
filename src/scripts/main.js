1355e70859824acd5c9651f796ef04717abb658c
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-09-12 23:48:40 +0300

    My weekend result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const prev = document.querySelector('.gallery__button--prev');
+const next = document.querySelector('.gallery__button--next');
+
+let position = 0;
+
+move(0);
+
+next.addEventListener('click', function() {
+  move(1);
+});
+
+prev.addEventListener('click', function() {
+  move(-1);
+});
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+
+  position = (position + count) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
