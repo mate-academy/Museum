80462058e5aa47f34047e8f989a25d569fcccd75
Roman Labonin <roman.labonin@gmail.com>
2021-12-08 00:12:27 +0200

    Rom Lab Museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__form-input');
+const textarea = document.querySelector('.contact-us__form-text');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  /* inputs.forEach(elem => elem.value = ''); */
+
+  textarea.value = '';
+});
