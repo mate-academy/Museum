658264822558c51ef1e4d1dad280653f21f82785
izzilowe <izzilowe7@gmail.com>
2021-12-08 21:41:53 +0200

    add contact-us

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-return-assign */
 'use strict';
+
+const form = document.querySelector('.subscribe__form');
+const inputs = document.querySelectorAll('.subscribe__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => elem.value = '');
+});
