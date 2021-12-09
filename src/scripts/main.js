f3d1cc3cdd352363758f97ade44a4482cfaafbad
Alex Ivanov <bcmp4p1@gmail.com>
2021-12-09 18:53:41 +0200

    100%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+/* eslint-disable */
+
+const form = document.querySelector('.follow__form');
+const inputs = document.querySelectorAll('.follow__email');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => elem.value = '');
+
+  textarea.value = '';
+});
