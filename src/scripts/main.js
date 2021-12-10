b63b5a362e1825c7b112f52e07b2d4e9610d82be
Bohdan Maksymiuk <bohdan.max@ukr.net>
2021-12-10 09:58:45 +0200

    prefinal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.submit__button');
+const inputs = document.querySelectorAll('.submit__button-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
