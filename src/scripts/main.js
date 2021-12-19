f74f7f15d79a53a3ab35d21e51c81529cc307c14
Viktor Bachynskyi <viktorbachinskiy@gmail.com>
2021-12-20 01:04:28 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+/* eslint-disable */
+
+const form = document.querySelector('.subscription__form');
+const inputs = document.querySelectorAll('.subscription__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => elem.value = '');
+
+  textarea.value = '';
+});
