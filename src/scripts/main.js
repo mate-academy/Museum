320ffda76699cdc24d8e7f6577392c58e2ddae66
Katruk Evgeny <zaiaz_@i.ua>
2022-02-09 11:29:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,1 @@
 'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
-
-const emailForm
-= document.getElementById('get-user-email');
-const emailInput
-= document.getElementById('btn-mail');
-
-emailForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  emailInput.value = '';
-});
