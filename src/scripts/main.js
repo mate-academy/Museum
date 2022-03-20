dcd8a528d13d0b4462ff3a885d1b0f4fb1fc8930
Max Nikiforov <maxxnikiforov@gmail.com>
2022-03-20 22:57:33 +0700

    lending Museum created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function stopDefAction(evt) {
+  document.getElementById('form').reset();
+  evt.preventDefault();
+}
+
+document.getElementById('form').addEventListener(
+  'submit', stopDefAction, false
+);
