7f034cc0e0e1af62ea428661e7e16a8012d9b285
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-06-05 17:14:07 +0300

    JS code was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const form = document.querySelector('.subscription__form');
+const emailInput = form.querySelector('.subscription__email');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  emailInput.value = '';
+});
+
+const body = document.querySelector('.page__body');
+const menuToggler = document.querySelector('.icon--menu');
+
+menuToggler.addEventListener('click', (event) => {
+  body.classList.add('page__body--no-overflow');
+});
+
+const menuLinks = document.querySelectorAll('.menu__link');
+
+for (const link of menuLinks) {
+  link.addEventListener('click', () => {
+    body.classList.remove('page__body--no-overflow');
+  });
+};
