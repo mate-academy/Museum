54173e654f6672d57b966792de38a40acca1dc36
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2022-07-12 21:06:52 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const form = document.querySelector('.subscription__form');
+const email = document.querySelector('.subscription__email');
+
+function scrollChange() {
+  if (window.location.hash === '#menu') {
+    body.style.overflow = 'hidden';
+  }
+
+  if (window.location.hash !== '#menu') {
+    body.style.overflow = '';
+  }
+}
+
+form.addEventListener('submit', () => {
+  window.location.hash = '';
+  email.value = '';
+});
+
+window.onhashchange = scrollChange;
+
+scrollChange();
