9938bb328ec1e9f8176855c5a0a976e07fcb9522
Dmytro Kretov <dmytro.kretov@gmail.com>
2022-07-14 16:01:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
+
+const form = document.querySelector('.subscribe__container');
+const inputs = document.querySelectorAll('.subscribe__container-input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
