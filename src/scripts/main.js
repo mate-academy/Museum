19d2ff1cd831510c5b1046a9ad07c66ddb046450
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-08-01 15:46:33 +0300

    added swiper library, improwed my slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
-'use strict';
+import Swiper from 'swiper/swiper-bundle';
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
