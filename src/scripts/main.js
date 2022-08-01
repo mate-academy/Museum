a15909fab8fc5f94da146d74cf4a2eb604ed2cb2
Synytsyn Denys <yungdena54@gmail.com>
2022-08-01 14:42:46 +0300

    added menu, fixed small issues in footer, gallery still on development stage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-'use strict';
+import Swiper from '../../node_modules/swiper/swiper-bundle';
+
+// eslint-disable-next-line strict, no-new
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  direction: 'horizontal',
+  loop: true,
+});
