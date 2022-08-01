6625cc2c511e8ba305574c7c57607ffa55f3b233
Max Kravchenko <max.kravchenko@gmail.com>
2022-08-01 21:22:32 +0300

    Add footer, connected swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-'use strict';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/swiper-bundle.min.css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// eslint-disable-next-line no-new
+// eslint-disable-next-line no-new
+new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
