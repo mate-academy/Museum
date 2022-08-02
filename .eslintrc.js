081451eb8889f4f053565a1b3610c1e039c03322
lizapolokhina <lizapolohina@gmail.com>
2022-08-02 15:11:09 +0200

    unfinished

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+}
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-'use strict';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+window.swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation],
+});
