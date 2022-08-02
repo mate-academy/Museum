497c34c4ebf80f1466d14935fa8fc7cc1a7a40f4
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-08-02 13:04:15 +0300

    add target attribute and fix scrolling under the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 import Swiper from 'swiper/swiper-bundle';
 // import Swiper styles
 import 'swiper/swiper-bundle.css';
