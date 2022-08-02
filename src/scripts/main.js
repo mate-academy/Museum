d1b36cf0c92c9e01c17b849bd4e92bc82e348a6f
Oleksii Pustovit <91pustovit91@gmail.com>
2022-08-02 10:38:57 +0300

    added footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-'use strict';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+
+  loop: true,
+  clickable: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+});
