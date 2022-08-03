806f4d5cb5f1eb24bcbe9f5424a315da94b6b85d
Artemii Pavlenko <artemiipav@gmail.com>
2022-08-03 19:00:39 +0300

    all is ready, need to fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-'use strict';
+import Swiper from 'swiper';
+
+import 'swiper/swiper-bundle.css';
+
+// eslint-disable-next-line no-new
+new Swiper('.swiper', {
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
