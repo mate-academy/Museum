e17fe9c6d62a342fea84f7cbfa52e19b9938da11
Oleksii Pustovit <91pustovit91@gmail.com>
2022-08-04 16:50:08 +0300

    fixes v5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,5 +12,18 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
-
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// eslint-disable-next-line max-len
+document.querySelector('#form-subscribe').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('form-subscribe').reset();
+}, false);
