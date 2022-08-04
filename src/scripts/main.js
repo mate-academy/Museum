10812202c6c19ad93ab9f9d27973644208a80258
Oleksii Slobodian <slobodan717@gmail.com>
2022-08-03 19:53:35 -0600

    Museum solution 1.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#headerMenu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const formSubmit = document.getElementById('formSubmit');
+
+formSubmit.addEventListener('click', function(event) {
+  event.preventDefault();
+});
