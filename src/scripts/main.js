151c263364e135b62cba07d482bdb597ba6eb6a2
Andriy Andrieiev <proph7000@gmail.com>
2022-09-01 20:11:09 +0300

    Museum without slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function handleClick(event) {
+  // if you are submitting a form (prevents page reload)
+  event.preventDefault();
+
+  const emailInput = document.getElementById('email');
+  // const nameInput = document.getElementById('name');
+  // const messageInput = document.getElementById('message');
+
+  // Send value to server
+  // console.log(emailInput.value);
+  // console.log(nameInput.value);
+  // console.log(messageInput.value);
+
+  // clear input field
+  emailInput.value = '';
+  // nameInput.value = '';
+  // messageInput.value = '';
+});
