3997f18d16aa3cfd5c2f0d64dc2d2687a84e56e1
Andriy Andrieiev <proph7000@gmail.com>
2022-09-04 13:28:11 +0300

    implemented clearing form fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const submit = document.getElementById('submit');
+const form = document.getElementById('email-form');
 
-submit.addEventListener('click', function handleClick(event) {
-  // if you are submitting a form (prevents page reload)
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const emailInput = document.getElementById('email');
-  // const nameInput = document.getElementById('name');
-  // const messageInput = document.getElementById('message');
-
-  // Send value to server
-  // console.log(emailInput.value);
-  // console.log(nameInput.value);
-  // console.log(messageInput.value);
-
-  // clear input field
-  emailInput.value = '';
-  // nameInput.value = '';
-  // messageInput.value = '';
+  event.target.reset();
 });
