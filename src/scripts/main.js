c721bddfef16a23d62f7532163eb7cce028a428c
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-09-07 12:26:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page_content-with-menu');
+  } else {
+    document.body.classList.remove('page_content-with-menu');
+  }
+});
