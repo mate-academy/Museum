f1485bb056475f347688f60d6d737fd8192cdde5
Erik Karaman <erikkaraman7@gmail.com>
2022-09-25 02:17:11 +0300

    fix form and navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,11 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function submitForm(evt) {
+  evt.preventDefault();
+}
+
+document.getElementById('send').addEventListener(
+  'click', submitForm, false
+);
