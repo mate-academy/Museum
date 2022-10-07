e33a521e307a932c168f8b8de4d804b10ce8a439
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-10-07 08:35:37 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.getElementById('my-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target[0].value = '';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
