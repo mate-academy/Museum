f087ca250855f6aab79a94da06fc9ff3035ab62e
Dima <d.maik2178@gmail.com>
2022-10-29 20:17:54 +0300

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,16 @@
-
 'use strict';
 
 const menu = document.querySelector('.header__nav');
 const openButton = document.querySelector('.header__burger');
 const closeButton = document.querySelector('.nav__close');
 const closeLink = document.querySelector('.nav__list--close');
+const subscriptionForm = document.querySelector('.subscription__form');
+const formInput = document.querySelector('.form__input');
+
+subscriptionForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formInput.value = '';
+});
 
 openButton.addEventListener('click', () => {
   menu.classList.add('header__nav--active');
