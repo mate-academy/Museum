ceb8345bab48aed3aba53c919c0f198cfb1ce2ec
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2022-11-03 16:29:35 +0200

    end landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,5 +7,5 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('.contact-us__form');
-form.addEventListener('submit', (e) => {  e.preventDefault(); form.reset();});
+const form = document.querySelector('.subscribe__form');
+form.addEventListener('submit', (e) => {e.preventDefault(); form.reset();});
