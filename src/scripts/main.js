b8a12639c2714d2f13bb3ac88a2b53e83e4bfba4
victor buldenko <gookidoo@gmail.com>
2022-11-05 14:25:51 +0300

    pre/done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.addEventListener('mouseover', e => {
+  const inputMail = document.querySelector('.follow__email');
+  const temp = inputMail.style.marginRight;
+
+  if (e.target === document.querySelector('.follow__button')) {
+    inputMail.style.marginRight = 0;
+    inputMail.style.borderRadius = '35px 0 0 35px';
+  }
+
+  e.target.addEventListener('mouseleave', () => {
+    inputMail.style.marginRight = temp;
+    inputMail.style.borderRadius = '';
+  });
+});
+
+document.querySelector('.follow__form').addEventListener('submit', e => {
+  e.preventDefault();
+  document.querySelector('.follow__form').reset();
+});
