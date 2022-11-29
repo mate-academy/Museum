d33408753cf33b28f3a900b976a37b52674269c6
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-11-30 01:26:07 +0200

    Gallery section with swiper was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
