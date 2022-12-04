422269eefac89e18cc72686f13fc93c61773ce37
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-12-04 02:40:46 +0200

    Contacts and footer sections was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Initialize swiper for Galery section -->>
+
 // eslint-disable-next-line no-unused-vars, no-undef
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
@@ -18,4 +20,29 @@ const swiper = new Swiper('.swiper', {
       spaceBetween: 30,
     },
   },
+});
+
+// Disable page refresh on form-submit -->>
+
+const form = document.querySelector('.contact-form__form');
+const inputs = document.querySelectorAll('.prevent-refresh');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
+
+// Disable scroll on menu-open -->>
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
