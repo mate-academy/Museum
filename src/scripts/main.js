a5528983e183c1a42f57c52e67ae5696856f3d37
Samir Mamedov <samirmamedoviphone@gmail.com>
2022-12-05 15:41:33 +0200

    More changes were done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
+/* eslint-disable no-undef */
 'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 1.1,
+  spaceBetween: 16,
+
+  zoom: {
+    maxRatio: 5,
+  },
+
+  breakpoints: {
+    460: {
+      slidesPerView: 2.1,
+      spaceBetween: 20,
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletClass: 'swiper-pagination-bullet',
+  },
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
