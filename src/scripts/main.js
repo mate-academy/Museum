9b2d529511108a03913fe20691f90609699060fc
Danil <dasha8854567@gmail.com>
2022-12-17 13:06:59 +0200

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const openNavButton = document.querySelector('.header__open-nav');
+const nav = document.querySelector('.nav');
+const navCloseButton = document.querySelector('.nav__close');
+const navLinks = [...document.querySelectorAll('.nav__link')];
+const closeNav = () => {
+  nav.classList.remove('nav--open');
+};
+
+const openNav = () => {
+  nav.classList.add('nav--open');
+};
+
+navLinks.forEach(navLink => navLink.addEventListener('click', closeNav));
+openNavButton.addEventListener('click', openNav);
+navCloseButton.addEventListener('click', closeNav);
