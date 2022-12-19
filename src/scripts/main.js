45f59814f68111ded292ae0b1934d593df333617
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2022-12-19 21:24:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/* eslint-disable no-undef */
+/* eslint-disable no-new */
+new Swiper('.slider__container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // grabCursor: true,
+  autoheight: true,
+  slidesPerView: 1,
+  loop: true,
+  keybord: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1280: {
+      enabled: false,
+    },
+  },
+});
