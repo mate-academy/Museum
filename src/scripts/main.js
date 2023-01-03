b387032988fba442e6c8962bc9a6a5b459041474
MarynaTovtyn <marina6782@gmail.com>
2023-01-03 17:22:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-'use strict';
+'use stict';
+
+const form = document.querySelector('.subscribe__form');
+const email = document.querySelector('.subscribe__email');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+form.addEventListener('submit', () => {
+  window.location.hash = '';
+  email.value = '';
+});
