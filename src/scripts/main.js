e2fea442dd1a8a61622b5d30acb845c25899cbe7
Oleksandr Maslo <98667890az@gmail.com>
2023-01-10 16:42:28 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,21 @@ document.addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu--mobile');
+  } else {
+    document.body.classList.remove('page__body--with-menu--mobile');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  const overlay = document.querySelector('.overlay');
+
+  if (window.location.hash === '#menu') {
+    overlay.classList.add('page__overlay-active');
+  } else {
+    overlay.classList.remove('page__overlay-active');
+  }
+});
