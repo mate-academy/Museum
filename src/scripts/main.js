baac155863e8d4fc2545d3448b863c7605640463
Dmytro Hubych <dh.hubych@gmail.com>
2023-01-16 17:52:41 +0200

    added Swiperjs to gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
