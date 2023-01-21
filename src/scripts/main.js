d90c0a3ace9727ba4e53472c4480cb74f1c3a370
Jaxxxon78 <jaxxxon78@ukr.net>
2023-01-21 13:16:35 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
