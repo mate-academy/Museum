163db92694602907adcc24019dd7088a570f86bc
Dmytro Yevtyeryev <begoodeee@gmail.com>
2023-01-26 00:59:15 +0200

    added lectureblock, added gallery block, added subscription block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const formSubmit = document.querySelector('#subscription-form');
+
+formSubmit.addEventListener('submit', formSubmitSuccess, false);
+
+function formSubmitSuccess(event) {
+  const warn = 'Дякуємо! Ви успішно підписались на наші новини!<br>';
+
+  document.getElementById('output-box').innerHTML = warn;
+
+  document.getElementById('subscription-form').reset();
+
+  event.preventDefault();
+}
