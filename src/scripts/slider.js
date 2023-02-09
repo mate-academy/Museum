cceb9e4087456afb86c191204881ba715306a831
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-02-09 11:49:47 +0200

    slider for mobile was added

---
+++ src/scripts/slider.js
@@ -0,0 +1,15 @@
+'use strict';
+// eslint-disable-next-line
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
