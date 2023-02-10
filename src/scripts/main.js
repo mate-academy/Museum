9343bf910e9c0d243e3b4db08609c30277472357
Yelyzaveta <molochiza@gmail.com>
2023-02-10 19:50:25 +0200

    museum 1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
+
+const swiper = new Swiper('.gallery__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  effect: 'slide',
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 16,
+
+  pagination: {
+    el: '.gallery__pagination',
+    clickable: true,
+  },
+});
