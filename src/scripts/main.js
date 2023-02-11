2e2c7868836f76bf1bf4557aafbe6fd508df6192
Yelyzaveta <molochiza@gmail.com>
2023-02-11 19:42:19 +0200

    museum 2nd commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,17 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('body--with-menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('body--with-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
 });
 
 const swiper = new Swiper('.gallery__swiper', {
