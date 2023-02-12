89695b7a0f0646fb22370fddae71e97838a1779c
Natalia Matash <natalimatash@gmail.com>
2023-02-12 14:24:14 +0200

    Created MENU section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,10 @@
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
+    document.querySelector('.page__menu').style.transform = 'translateY(0%)';
+    window.scrollTo({ behavior: 'smooth' });
   } else {
     document.body.classList.remove('page__body--with-menu');
+    document.querySelector('.page__menu').style.transform = 'translateY(-100%)';
   }
 });
