c353db1c561a62a863d52fcbec922ea689ca681a
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-02-13 15:36:47 +0200

    created landing page "museum"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const formSubmit = document.querySelector('#form');
+
+formSubmit.addEventListener('submit', formSubmitSuccess, false);
+
+function formSubmitSuccess(event) {
+  const warn = 'Дякуємо! Ви успішно підписались на наші новини!<br>';
+
+  document.getElementById('output-box').innerHTML = warn;
+
+  document.getElementById('subscription__form').reset();
+
+  event.preventDefault();
+}
