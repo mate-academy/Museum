23de80bc86044b406f78a260c4382ff8a8c0e6a0
Natalia Matash <natalimatash@gmail.com>
2023-02-14 11:56:32 +0200

    Created slider in Gallety section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,45 @@ window.addEventListener('hashchange', () => {
     document.querySelector('.page__menu').style.transform = 'translateY(-100%)';
   }
 });
+
+let isDown = false;
+let startX;
+let scrollLeft;
+const slider = document.querySelector('.slider__items');
+
+const end = () => {
+  isDown = false;
+  slider.classList.remove('active');
+};
+
+const start = (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+
+const move = (e) => {
+  if (!isDown) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  const dist = (x - startX);
+
+  slider.scrollLeft = scrollLeft - dist;
+};
+
+(() => {
+  slider.addEventListener('mousedown', start);
+  slider.addEventListener('touchstart', start);
+
+  slider.addEventListener('mousemove', move);
+  slider.addEventListener('touchmove', move);
+
+  slider.addEventListener('mouseleave', end);
+  slider.addEventListener('mouseup', end);
+  slider.addEventListener('touchend', end);
+})();
