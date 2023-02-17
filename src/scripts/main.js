6013b3545af567d543b4d35434fb2a24e7e1800b
Kostia Hudzenko <kostya.gudzenko1991@gmail.com>
2023-02-17 11:34:01 +0200

    Section events was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  window.location.hash === '#menu'
+    ? document.body.classList.add('page__body--with-menu')
+    : document.body.classList.remove('page__body--with-menu');
+});
