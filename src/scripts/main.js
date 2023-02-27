6ee857682a41af21f4d17342d493f8f2437e7a36
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-02-27 16:18:25 +0200

    Fixed! but still problems with button arrow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const pageReload = document.querySelector('#form-reload');
+
+pageReload.addEventListener('submit', function formReload(event) {
+  event.preventDefault();
+
+  pageReload.reset();
+});
