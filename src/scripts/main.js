33506f2da47997b67385535af4ba32dcb704e81d
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-02-27 18:53:46 +0200

    Fixed! Thanx god i found icon button with arrow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const pageReload = document.querySelector('#form-reload');
 
 pageReload.addEventListener('submit', function formReload(event) {
