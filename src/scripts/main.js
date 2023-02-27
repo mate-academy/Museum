b07a3c6bd2b53e1c251ffebe37a5524b4c254e3b
Olexsandr Fedorov <dfkd81@gmail.com>
2023-02-27 11:03:57 +0200

    update block menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.onhashchange = (e) => {
+  const rez = e.newURL.includes('#menu')
+    ? document.body.classList.add('page--background')
+    : document.body.classList.remove('page--background');
+
+  return rez;
+};
