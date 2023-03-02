1a9033f4646c83d17b93c7c508c32ad003394e9c
Illia0308 <iluxa.stalker@gmail.com>
2023-03-02 23:15:07 +0100

    Museum landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const makeDefault = document.querySelector('.subscription__form');
+
+makeDefault.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  document.getElementsByName('email')[0].value = '';
+});
