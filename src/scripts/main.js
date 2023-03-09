76ed4f451790b99671921182da787e01392604c4
Nazar Vorona <voron1765@gmail.com>
2023-03-09 05:09:32 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const form = document.querySelector('.follow__form');
+const inputs = document.querySelectorAll('.follow__form--input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+});
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body-with--menu');
+  } else {
+    document.body.classList.remove('page__body-with--menu');
+  }
+});
