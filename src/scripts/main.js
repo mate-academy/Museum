249cc744098a5070894900cb5c4578989a373e85
Daria <hamdsad8@gmail.com>
2023-03-09 10:11:16 +0200

    added most resent layout (needs adaptation for different formats)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const checkIsMenuOpen = () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+};
+
+window.addEventListener('hashchange', () => {
+  checkIsMenuOpen();
+});
