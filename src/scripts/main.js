75a405a544ccb8ff1c0b0bb1a66a2c395aa46643
Daria <hamdsad8@gmail.com>
2023-03-10 14:49:50 +0200

    added finale changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,28 @@ const checkIsMenuOpen = () => {
 window.addEventListener('hashchange', () => {
   checkIsMenuOpen();
 });
+
+const form = document.querySelector('.follow__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+  window.alert('You are subscribed!');
+});
+
+window.addEventListener('scroll', () => {
+  const reveals = document.querySelectorAll('.section--reveal');
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+});
