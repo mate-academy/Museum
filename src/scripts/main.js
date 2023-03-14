aba520649dbf83f6d1273d61a3b5d130f973ca5f
Sergiy Nikiforov <nikiforov.sg@gmail.com>
2023-03-14 15:18:46 +0200

    completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// const menu = document.querySelector('.menu-dimmer');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu-dimmer--active', 'page__body--with-menu');
+  } else {
+    document.body.classList.remove(
+      'menu-dimmer--active',
+      'page__body--with-menu'
+    );
+  }
+});
+
+const form = document.querySelector('.subscription__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
