256ef165e372b3e78ebccbd3508b73c496feb3a9
Andrey Bikir <bikir.a.v@gmail.com>
2023-03-24 01:55:49 +0200

    Museum MAMU

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+// menu
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// form reload
+const form = document.getElementById('form-reload');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
+
+// slider
+const swiper = new Swiper('.slider', {
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+
+  reakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
