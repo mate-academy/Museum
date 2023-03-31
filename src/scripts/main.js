022e9813f12c564e402be2faf36c34ccdd44e426
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-03-31 18:06:22 +0300

    subscription block was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const swiper = new swiper('.swiper', {
+// const swiper = new swiper('.swiper', {
 
-  direction: 'horizontally',
-  loop: true,
+//   direction: 'horizontally',
+//   loop: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+const form = document.querySelector('.subscription__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__with-menu');
+  } else {
+    document.body.classList.remove('page__with-menu');
+  }
 });
