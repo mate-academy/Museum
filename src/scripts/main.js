c116e228b7fbaf2dd1d9520aa193820d2c84e42c
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-04-08 15:01:00 +0300

    landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,8 @@
-// document.addEventListener('click', (ev) => {
-//   if () {
-//     document.getElementsByClassName('active').add('active')
-//   }
-// })
-
 // eslint-disable-next-line strict
-// function clickDot() {
-//   document.addEventListener('click').classList.add('.active');
-// }
-
-// eslint-disable-next-line strict
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
