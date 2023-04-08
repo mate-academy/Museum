ea9e5374d4ed999e212a26c5084e84da18ad4db4
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-04-08 14:40:12 +0300

    add landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-'use strict';
+// document.addEventListener('click', (ev) => {
+//   if () {
+//     document.getElementsByClassName('active').add('active')
+//   }
+// })
+
+// eslint-disable-next-line strict
+// function clickDot() {
+//   document.addEventListener('click').classList.add('.active');
+// }
+
+// eslint-disable-next-line strict
