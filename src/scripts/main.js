b42e52e8afd2a513d6082347b108cb83509813c8
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2023-04-18 19:00:03 +0300

    fix sum bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,3 +32,39 @@ new Swiper('.slider__container', {
 document.querySelector('.subscribe__button').onclick = function(e) {
   document.querySelector('.subscribe__input').value = '';
 };
+
+// при відкритті меню приховуємо прокрутку сторінки
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', () => {
+  document.body.classList.toggle('no-scroll');
+});
+
+const form = document.getElementById('form');
+// const subscribeButton = document.getElementById('subscribe');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (form.checkValidity()) { // перевіряємо форму на валідність
+    const formData = new FormData(form);
+    // створюємо об'єкт FormData для збору даних форми
+    const xhr = new XMLHttpRequest();
+    // створюємо новий об'єкт XMLHttpRequest
+
+    xhr.open('POST', 'url/to/server');
+    // встановлюємо метод та URL для відправки Ajax-запиту
+
+    xhr.onload = function() {
+      message.textContent = 'Форма успішно відправлена';
+    };
+
+    xhr.onerror = function() {
+      message.textContent = 'Сталась помилка при відправленні форми';
+    };
+    xhr.send(formData);
+  } else {
+    message.textContent = 'Будь ласка, введіть правильний email';
+  }
+});
