e7c6ef10c0bf7cf6a82cd2bd273e66372dee55ef
Liza Strykharchuk <ol220681vol@gmail.com>
2023-04-26 20:59:19 +0300

    museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
+
+// const imgs = document.querySelectorAll('.gallery__phone');
+// const dots = document.querySelectorAll('.gallery__dot');
+
+// let currentImg = 0;
+
+//  function changeSlide(n) {
+//   // for (let i = 0; i < imgs.length; i++) {
+//   //   imgs[i].style.opacity = 0;
+//   //   dots[i].className = dots[i].className.replace(' active', '');
+//   // }
+
+//   // currentImg = n;
+
+//   // imgs[currentImg].style.opacity = 1;
+//   // dots[currentImg].className += ' active';
+
+//   console.log(555);
+// }
+
+// module.exports = changeSlide;
