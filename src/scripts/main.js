4ec2cdfa5d246729d6e2718e69732181c28ccabd
Andrey Rudenko <rudenko.and@gmail.com>
2023-05-10 12:56:20 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+/* global Swiper */
+
+// menu
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// form reload
+const form = document.getElementById('form-reload');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
+
+// slider
+const swiper = new Swiper('.slider', {
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+
+  reakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+swiper.enable();
