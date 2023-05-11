01d48c6d0de701da880502e83c906b3044af2e65
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-11 14:21:13 +0300

    'js'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,31 +15,32 @@ form.addEventListener('submit', (e) => {
   e.target.reset();
 });
 
-let slideIndex = 1;
+// let slideIndex = 1;
 
-showSlides(slideIndex);
+// currentSlide(slideIndex);
 
-function showSlides(n) {
-  let i;
-  const slides
-  = document.getElementsByClassName('gallery__slideshow-container__mySlides');
-  const dots = document.getElementsByClassName('gallery__dot');
+// function currentSlide(n) {
+//   let i;
+//   const slides
+//   = document.getElementsByClassName
+// ('gallery__slideshow-container__mySlides');
+//   const dots = document.getElementsByClassName('gallery__dot');
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
 
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].className += ' active';
+// }
