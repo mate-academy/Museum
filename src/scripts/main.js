cf9bcf8a21c300f6ad2f67d457077f98d093722f
Sergey Arnautov <sirius9312@ukr.net>
2023-05-12 22:36:27 +0300

    Museum-LANDING

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.querySelector('input[type="text"]').disabled = true;
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.querySelector('input[type="text"]').disabled = false;
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
