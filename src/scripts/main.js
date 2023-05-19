76cf6d49f78bed0b83223ff2611775e60466dcde
Valera Viachalo <valeravyachalo@gmail.com>
2023-05-19 19:40:39 +0300

    menu task solution and input form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,13 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.subscription__form');
+
+form.addEventListener('submit', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+  form.reset();
+  alert('Ваша підписка успішно оформлена!');
+};
