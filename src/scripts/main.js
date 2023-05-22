dbcf4ef847ea79cbcecb612e2c42b2ef2b8073e9
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-05-22 12:28:26 +0100

    museum landing completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,31 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.getElementById('form-reload');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
+
+// eslint-disable-next-line no-undef,no-unused-vars
+const swiper = new Swiper('.slider', {
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+}
+);
