e443369a3ed6648eaba12d86ae71defd1414d890
Eugene Tykhonov <sargosbest@gmail.com>
2023-05-23 04:43:37 +0300

    My Museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-opener');
+  } else {
+    document.body.classList.remove('page__body--menu-opener');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
+
+/* global Swiper */
+
+const swiper = new Swiper('.slider', {
+  loop: true,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+swiper.enable();
