91ad145e11b8239a20bbf13699043eaf17cfe5d6
Nazar Yants <nazyants@gmail.com>
2023-05-26 18:53:57 +0300

    namu-page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function swiperFunc() {
+  // eslint-disable-next-line no-undef
+  const swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    centeredSlides: true,
+
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  return swiper;
+}
+
+swiperFunc();
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
