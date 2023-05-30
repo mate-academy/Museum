d57dac02d6443064b861f524b6be720704ebb29b
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-30 14:58:59 +0300

    'script'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,32 +14,3 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.reset();
 });
-
-let slideIndex = 1;
-
-currentSlide(slideIndex);
-
-function currentSlide(n) {
-  // let i;
-  const slides = document.getElementsByClassName('gallery__mySlides');
-  const dots = document.getElementsByClassName('gallery__dot');
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (let b = 0; b < slides.length; b++) {
-    slides[b].style.display = 'none';
-  }
-
-  for (let j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(' active', '');
-  }
-
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
