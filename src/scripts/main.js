6d09b3c26740cf0a93a71567df36208ccbd42d31
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-06-06 00:16:43 +0200

    header and other 4 blocks was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const menuOpenButton = document.querySelector('.icon--burger');
+const menuBlock = document.querySelector('.menu');
+const submitButton = document.querySelector('.subscription__submit');
+
+menuOpenButton.addEventListener('click', () => {
+  menuBlock.style.cssText = 'transition: transform 1s; transform:none';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    menuBlock.style.cssText = 'transition: transform 1s; transform:none;';
+    document.body.style.cssText = 'overflow-y: hidden;';
+  } else {
+    const cssText = 'transition: transform 1s; transform: translateY(-100%)';
+
+    menuBlock.style.cssText = cssText;
+    document.body.style.cssText = 'overflow-y: scroll;';
+  }
+});
+
+submitButton.addEventListener('click', (e) => {
+  document.querySelector('form').reset();
+  e.preventDefault();
+});
