86732ab45fe1e90968a96567ff2826feb334cadb
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-06-13 09:30:26 +0300

    Added scroll top btn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const scrollTopBtn = document.getElementById('myBtn');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000
+    || document.documentElement.scrollTop > 1000) {
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// eslint-disable-next-line no-unused-vars
+scrollTopBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
 const menu = document.querySelector('.menu');
 const burgerMenu = document.querySelector('.header__burger-menu');
 const body = document.getElementById('home');
