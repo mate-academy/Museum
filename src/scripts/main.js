f6b0617200a6a1efc8aec436c9b1f480a49f357f
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-06-16 14:51:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  const menu = document.querySelector('.header__menu');
+
+  if (window.location.hash === '#menu-burger') {
+    menu.classList.add('header__menu--show');
+  } else {
+    menu.classList.remove('header__menu--show');
+  }
+});
