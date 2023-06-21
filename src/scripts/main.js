70358309f9f2712bb9b2d05b87e2b5f117452786
Diana Tuz <dianatuz12@gmail.com>
2023-06-21 15:38:42 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// $(document).ready(function() {
+//   $('.slider').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+// });
