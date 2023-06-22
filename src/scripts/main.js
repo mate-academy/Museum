b1134433ca66c92e171a7d31ba49e2c95d0caf16
Diana Tuz <dianatuz12@gmail.com>
2023-06-22 18:49:54 +0300

    fix5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const openMenu = document.querySelector('.main-page__menu');
-const closeMenu = document.querySelector('.menu__close');
-const body = document.querySelector('.body');
-
-openMenu.addEventListener('click', function() {
-  body.classList.add('no-active');
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body__no-active');
+  } else {
+    document.body.classList.remove('body__no-active');
+  }
 });
 
-closeMenu.addEventListener('click', function() {
-  body.classList.remove('no-active');
-});
+// eslint-disable-next-line no-undef
+new Swiper('.slider', {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+}).enable();
