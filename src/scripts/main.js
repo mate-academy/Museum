94d744fa546898174ac2669b140d9d95ee335461
yaroslavdashko <yaroslav.dashkoa@gmail.com>
2023-06-26 19:53:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+        document.body.classList.add('page__body--with-menu');
+    } else {
+        document.body.classList.remove('page__body--with-menu');
+    }
+});
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    document.querySelector('form').reset();
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
