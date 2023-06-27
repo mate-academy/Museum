a4d936cd6c77f88c07b28bfc91bf652ca4468e87
Daria <hamdsad8@gmail.com>
2023-06-27 12:22:08 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,19 +20,3 @@ form.addEventListener('submit', function(event) {
   form.reset();
   window.alert('You are subscribed!');
 });
-
-window.addEventListener('scroll', () => {
-  const reveals = document.querySelectorAll('.section--reveal');
-
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add('active');
-    } else {
-      reveals[i].classList.remove('active');
-    }
-  }
-});
