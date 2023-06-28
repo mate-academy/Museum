c7d9f28ab543492e8aeeec41a76846bd17d62572
Mari <kap.mebel.a@gmail.com>
2023-06-28 14:47:29 +0300

    end

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,6 @@ window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remov('page__body--with-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
