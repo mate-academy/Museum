692020bbdaebcc092e474204b307d5ef553e14e2
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-07-01 11:50:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,9 @@ window.addEventListener('hashchange', () => {
     menu.classList.remove('header__menu-content--show');
   }
 });
+
+window.addEventListener('submit', (event) => {
+  document.getElementById('email').value = '';
+
+  event.preventDefault();
+});
