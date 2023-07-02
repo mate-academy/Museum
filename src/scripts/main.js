673b7c542b02a665e080b31a3afaa163ab8e2625
Andrew Rich <andy.rich3a@gmail.com>
2023-07-02 18:34:26 +0300

    add js animation to menu button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const icons = document.querySelectorAll('.icon');
+
+icons.forEach(icon => {
+  icon.addEventListener('click', (event) => {
+    for (const ic of icons) {
+      ic.classList.toggle('open');
+    }
+  });
+});
