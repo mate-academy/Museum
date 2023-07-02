40141672d33c91ed69b9558da4bf0acd187b3fc2
Andrew Rich <andy.rich3a@gmail.com>
2023-07-02 22:18:39 +0300

    implement events block / add nav animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const icons = document.querySelectorAll('.icon');
+const links = document.querySelectorAll('.nav__link');
 
 icons.forEach(icon => {
   icon.addEventListener('click', (event) => {
     for (const ic of icons) {
       ic.classList.toggle('open');
+    }
+  });
+});
+
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    for (const ic of icons) {
+      ic.classList.remove('open');
     }
   });
 });
