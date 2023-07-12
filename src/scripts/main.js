89489df04799112623fd4b5a9f8034dc439784b0
Max Oleksyuk <maksoleksyuk@gmail.com>
2023-07-12 17:03:46 +0300

    fix menu on scrolling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const burger = document.querySelector('.top__burger');
+const body = document.querySelector('.body');
+const closes =  document.querySelector('.top__close');
+const link1 =  document.querySelector('.item-about');
+const link2 =  document.querySelector('.item-gallery');
+const link3 =  document.querySelector('.item-events');
+const link4 =  document.querySelector('.item-contacts');
+const btn = document.querySelector('.tickets__row');
+
+burger.addEventListener('click', () => {
+    body.classList.add('lock');
+  });
+
+closes.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
+
+btn.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
+
+link1.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
+link2.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
+link3.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
+link4.addEventListener('click', () => {
+  body.classList.remove('lock');
+});
 const swiper = new Swiper('.swiper', {
 
   slidesPerView: 1,
