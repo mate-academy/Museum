6125e82b283d05bb52fae68e21e02c1e71deddc2
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-07-25 19:01:16 +0300

    mobile almost done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const menu = document.getElementById('menu');
+const page = document.querySelector('.page__body');
+const pageWrapper = document.querySelector('.page__wrapper');
+const galleryGrid = document.querySelector('.gallery__grid');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    menu.classList.add('page__menu--target');
+    page.classList.add('page__body--overflow-hidden');
+    pageWrapper.classList.add('page__wrapper--on');
+  } else {
+    menu.classList.remove('page__menu--target');
+    page.classList.remove('page__body--overflow-hidden');
+    pageWrapper.classList.remove('page__wrapper--on');
+  }
+});
+;
+
+galleryGrid.style.width
+  = String((document.documentElement.clientWidth - 2 * 20)
+  * 4 + (16 * 3)) + 'px';
