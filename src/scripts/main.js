224f990d657762a6af8f33f2322e98bde64e23dc
Oleksandr Rudiuk <Alvlru629@gmail.com>
2023-07-30 21:13:26 +0300

    mueseum_first_edition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const sliderContent = document.querySelector('.slider__content');
+const pointerButton1 = document.querySelector('.pointer__button--1');
+const pointerButton2 = document.querySelector('.pointer__button--2');
+const pointerButton3 = document.querySelector('.pointer__button--3');
+const pointerButton4 = document.querySelector('.pointer__button--4');
+
+document.querySelector('.pointer__button--1')
+  .addEventListener('click', function() {
+    sliderContent.style.transform = 'translateX(0%)';
+    pointerButton1.style.backgroundColor = '#1a5a4c';
+    pointerButton2.style.backgroundColor = '#e0e0e0';
+    pointerButton3.style.backgroundColor = '#e0e0e0';
+    pointerButton4.style.backgroundColor = '#e0e0e0';
+  });
+
+document.querySelector('.pointer__button--2')
+  .addEventListener('click', function() {
+    sliderContent.style.transform = 'translateX(-20%)';
+    pointerButton2.style.backgroundColor = '#1a5a4c';
+    pointerButton1.style.backgroundColor = '#e0e0e0';
+    pointerButton3.style.backgroundColor = '#e0e0e0';
+    pointerButton4.style.backgroundColor = '#e0e0e0';
+  });
+
+document.querySelector('.pointer__button--3')
+  .addEventListener('click', function() {
+    sliderContent.style.transform = 'translateX(-40%)';
+    pointerButton3.style.backgroundColor = '#1a5a4c';
+    pointerButton2.style.backgroundColor = '#e0e0e0';
+    pointerButton1.style.backgroundColor = '#e0e0e0';
+    pointerButton4.style.backgroundColor = '#e0e0e0';
+  });
+
+document.querySelector('.pointer__button--4')
+  .addEventListener('click', function() {
+    sliderContent.style.transform = 'translateX(-60%)';
+    pointerButton4.style.backgroundColor = '#1a5a4c';
+    pointerButton2.style.backgroundColor = '#e0e0e0';
+    pointerButton3.style.backgroundColor = '#e0e0e0';
+    pointerButton1.style.backgroundColor = '#e0e0e0';
+  });
