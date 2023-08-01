2d08ece108b91e0504cc8b674f9fc0063cc7b787
Stanislav Kohut <stako211@gmail.com>
2023-08-02 00:08:11 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const $ = window.$;
+
+$(document).ready(function() {
+  const h = $('#menu').outerHeight();
+
+  document.querySelector('.menu').style.setProperty('--height', h + 'px');
+});
