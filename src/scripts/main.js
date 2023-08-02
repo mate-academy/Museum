73172d0b8ddfd3fa037ee45a3170017118e27794
Stanislav Kohut <stako211@gmail.com>
2023-08-02 23:45:13 +0300

    fix menu-shadow, logo it`s link, change positin logo and image in header for full hd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,22 @@
 
 const $ = window.$;
 
-$(document).ready(function() {
-  const h = $('#menu').outerHeight();
+// $(document).ready(function() {
+//   const h = $('#menu').outerHeight();
 
-  document.querySelector('.menu').style.setProperty('--height', h + 'px');
+//   document.querySelector('.menu').style.setProperty('--height', h + 'px');
+// });
+
+$(document).ready(function() {
+  function checkWidth() {
+    const h = $('#menu').outerHeight();
+
+    document.querySelector('.menu').style.setProperty('--height', h + 'px');
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function() {
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
 });
