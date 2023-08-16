ce4587a2667670c4bc63e18d4db58a268c870098
Mykola Huryn <mukola197022@gmail.com>
2023-08-16 22:56:06 +0200

    museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const form = document.querySelector('.bottom-section__form');
+
+const sendButton = document.querySelector(
+  '.bottom-section__form--vector'
+);
+
+sendButton.addEventListener('click', e => {
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
