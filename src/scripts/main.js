a8131a6d83df519240097811a67f27eb02a39e48
Yuriy Titov <yuranus@gmail.com>
2023-08-17 11:36:12 +0300

    fix: solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const slider = document.querySelector('.gallery__slider');
+const circles = document.querySelectorAll('.gallery__circle');
+
+circles.forEach((circle, index) => {
+  circle.addEventListener('click', () => {
+    const activeCircle = document.querySelector('.gallery__circle--active');
+
+    activeCircle.classList.remove('gallery__circle--active');
+    circle.classList.add('gallery__circle--active');
+
+    slider.style.transform = `translate(${index * -25}%)`;
+  });
+});
+
+const form = document.querySelector('.subscribe__form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  form.reset();
+});
