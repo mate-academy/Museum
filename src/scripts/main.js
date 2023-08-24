624965f3c6b8eed784410ba124f801516045a832
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-08-24 21:22:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const menu = document.querySelector('.icon--menu');
+const close = document.querySelector('.icon--close');
+const menuItem = document.querySelector('.menu__items');
+
+menu.addEventListener('click', (event) => {
+  page.classList.add('not-active');
+});
+
+close.addEventListener('click', (event) => {
+  page.classList.remove('not-active');
+});
+
+menuItem.addEventListener('click', (event) => {
+  page.classList.remove('not-active');
+});
