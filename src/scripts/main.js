99ff3c0064128aa8e550a9fd6fb17daeaaaf5363
Leonid Krainik <rialleons@gmail.com>
2023-08-28 19:14:58 +0300

    Add slider gallery on mobile and tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const slides = document.querySelectorAll('.gallery__image--mobile');
+const dots = document.querySelectorAll('.gallery__dot');
+
+showSlides(0);
+
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener('click', () => {
+    showSlides(i);
+  });
+}
+
+function showSlides(n) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.add('gallery__image--hide');
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('gallery__dot--active');
+  }
+
+  slides[n].classList.remove('gallery__image--hide');
+  dots[n].classList.add('gallery__dot--active');
+
+  if (document.body.clientWidth >= 640) {
+    slides[n + 1].classList.remove('gallery__image--hide');
+    dots[n].classList.add('gallery__dot--active');
+  }
+}
