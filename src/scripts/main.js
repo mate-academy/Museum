781f39cef47346a32bf0dfa170a27f905a1c69bd
yaroslavdashko <yaroslav.dashkoa@gmail.com>
2023-08-29 23:21:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
