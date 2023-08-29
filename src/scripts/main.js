d3948557e0952639e2cf903df604afa81efb71eb
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2023-08-29 22:53:13 +0300

    remove all comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,66 +1,11 @@
 'use strict';
 import Swiper from 'swiper/swiper-bundle.min.mjs';
-//  slider
-
-// let currentIndex = 0;
-// let interval;
-
-// function switchSlide() {
-//   currentIndex = (currentIndex + 1) % 4;
-//   document.getElementById(`radio${currentIndex + 1}`).checked = true;
-// }
-
-// function startSlider() {
-//   if (interval) {
-//     stopSlider();
-//   }
-
-//   interval = setInterval(switchSlide, 3000);
-// }
-
-// function stopSlider() {
-//   // currentIndex = 0;
-//   clearInterval(interval);
-// }
-
-// function checkWindowSize() {
-//   if (window.innerWidth < 1280) {
-//     startSlider();
-//   } else {
-//     stopSlider();
-//   }
-// }
-
-// checkWindowSize();
-
-// window.addEventListener('resize', () => {
-//   if (interval) {
-//     document.getElementById('radio1').checked = true;
-//     currentIndex = 0;
-//     stopSlider();
-//     checkWindowSize();
-//   }
-//   checkWindowSize();
-// });
-
-// document.querySelectorAll
-// ('input[name="radio-btn"]').forEach((radio, index) => {
-//   radio.addEventListener('click', () => {
-//     stopSlider();
-//     currentIndex = index;
-
-//     setTimeout(() => {
-//       checkWindowSize();
-//     }, 4000);
-//   });
-// });
 
 // swiper
-let swiper = null; // Оголошуємо змінну для зберігання об'єкта свайпера
+let swiper = null;
 
 function initSwiper() {
   swiper = new Swiper('.swiper', {
-    // Optional parameters
     loop: true,
     spaceBetween: 16,
     grabCursor: true,
@@ -71,14 +16,8 @@ function initSwiper() {
         slidesPerView: 2,
         spaceBetween: 20,
       },
-
-      // 1280: {
-      //   slidesPerView: 2,
-      //   spaceBetween: 30,
-      // },
     },
 
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -98,16 +37,15 @@ function initSwiper() {
 function checkScreenWidth() {
   if (window.innerWidth < 1280) {
     if (!swiper) {
-      initSwiper(); // Ініціалізуємо свайпер, якщо його ще не було створено
+      initSwiper();
     }
   } else {
     if (swiper) {
-      swiper.destroy(); // Знищуємо свайпер, якщо він вже існує
-      swiper = null; // Очищаємо змінну об'єкта свайпера
+      swiper.destroy();
+      swiper = null;
     }
   }
 }
 
-// Викликаємо перевірку при завантаженні сторінки та при зміні розміру вікна
 window.addEventListener('load', checkScreenWidth);
 window.addEventListener('resize', checkScreenWidth);
