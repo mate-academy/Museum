1c0059fe03d83b5aa9fdbf54e59493e19f787e89
BodyaKuts <mkkbodya@gmail.com>
2023-09-04 18:17:32 +0300

    implemented museum landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.slider__btn');
+const submitBtn = document.getElementById('submitBtn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('slider__btn--active'));
+    button.classList.add('slider__btn--active');
+  });
+});
+
+submitBtn.addEventListener('click', () => {
+  const email = document.getElementById('email');
+
+  if (email.checkValidity()) {
+    // eslint-disable-next-line no-undef
+    location.href = '';
+    email.value = '';
+  };
+});
