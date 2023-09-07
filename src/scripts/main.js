ca1a1e15cbc53f76d9d38a0f80bebd92fd905caa
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-09-07 13:40:23 +0300

    added animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('show');
+    }
+  });
+}
+
+const options = { threshold: [0.2] };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.image-animation');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
