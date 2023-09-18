a87139b061628d172a89056090d4031fb78a553a
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2023-09-19 01:22:06 +0300

    remake slider by swiper, fix close button for menu, disable content on menu open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,13 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+/* eslint-disable no-unused-vars */
+const swiper = new Swiper('.galery__slider', { // eslint-disable-line
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+/* eslint-enable no-unused-vars */
