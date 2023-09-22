2af9f53be0ba0bbf2afdf4851618e4accb385434
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2023-09-23 01:05:38 +0300

    fix v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.getElementById('form');
+const userEmail = document.getElementById('user-email');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  userEmail.value = '';
+});
