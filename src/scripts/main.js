fdccd6980da7cd2191b928fe30497084f403ee57
dima.rogkov <dima.rogkov@cheitgroup.com>
2023-10-01 13:52:04 +0300

    clean project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable max-len */
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  // ===============================
+  // STATE
+  // ===============================
+  const state = {
+    loader: {
+      element: document.querySelector('.loader'),
+      timeReady: 1200,
+    },
+  };
+
+  // ===============================
+  // LOADER
+  // ===============================
+  const loaderInit = ({ element, timeReady }) => {
+    if (!element) {
+      return;
+    }
+
+    setTimeout(() => document.body.classList.add('body--ready'), timeReady);
+  };
+  loaderInit(state.loader);
+});
