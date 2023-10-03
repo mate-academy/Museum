1bc8869a4d16c57e6915fe65d58589b98dcfeeeb
Your Name <renatabdrah001@gail.com>
2023-10-03 12:32:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
-// 'use strict';
+'use strict';
 
-// const page = document.querySelecrot('.page');
-// const menu = page.querySelecrot('.page__menu');
+// const menu = document.querySelector('.menu');
+// const pageBody = document.querySelector('.page__body');
 
-// menu.addEventListener('click', () => {
-//   page.classList.toggle('page__mask');
+// menu.addEventListener('click', function() {
+//   page.classList.toggle('page__body--with-menu');
 // })
+
+// console.log(menu);
+// console.log(pageBody);
+
+// menu.addEventListener('click', function() {
+//   const hasClass = pageBody.classList.contains('page__body--with-menu');
+
+//   if (!hasClass) {
+//       pageBody.classList.add('page__body--with-menu');
+//   } else {
+//       pageBody.classList.remove('page__body--with-menu');
+//   }
+// });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
