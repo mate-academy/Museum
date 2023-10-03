a3cf04b57460539668b419357c10ba7cdc578ad2
Your Name <renatabdrah001@gail.com>
2023-10-03 11:12:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-'use strict';
+// 'use strict';
+
+// const page = document.querySelecrot('.page');
+// const menu = page.querySelecrot('.page__menu');
+
+// menu.addEventListener('click', () => {
+//   page.classList.toggle('page__mask');
+// })
