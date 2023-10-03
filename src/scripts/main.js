ff1d974fe347cf0c1f64205e31179776ec7ba360
Wita-Shchurko <vitaliya.mememe@gmail.com>
2023-10-03 22:15:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const menu = document.getElementById('menu');
+const menuButton = document.querySelector('.header__menu');
+const overflov = document.querySelector('.page__overflov');
+const navLink = document.querySelectorAll('.nav__link');
+const scrollBtn = document.getElementById('scroll');
+
+function toggleMenu(element) {
+  element.addEventListener('click', () => {
+    menu.classList.toggle('page__menu-active');
+    menuButton.classList.toggle('header__menu-active');
+    overflov.classList.toggle('page__overflov-active');
+  });
+}
+
+toggleMenu(menuButton);
+navLink.forEach(x => toggleMenu(x));
+
+const scrollBtnDisplay = function() {
+  window.scrollY > window.innerHeight
+    ? scrollBtn.classList.add('page__scroll--active')
+    : scrollBtn.classList.remove('page__scroll--active');
+};
+
+window.addEventListener('scroll', scrollBtnDisplay);
