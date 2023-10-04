0d650ddb5a7713158e9b41f44b3b400504b61477
boytrend0108 <boytrend@gmail.com>
2023-10-04 20:39:01 +0300

    mobile vers 70% complete

---
+++ src/scripts/slider.js
@@ -0,0 +1,24 @@
+'use strict';
+debugger
+
+const sliderImages = document.querySelector('.slider__images');
+const sliderButtons = document.querySelectorAll('.slider__button-wr');
+
+sliderButtons.forEach(el => {
+  el.onclick = moveSlider;
+});
+
+function moveSlider({ currentTarget }) {
+  const num = currentTarget.getAttribute('data-image-num');
+
+  sliderImages.style.transform = `translateX(-${280 * num}px)`;
+  markButton(num);
+};
+
+function markButton(num) {
+  document.querySelector('.slider__button--active')
+    .classList.remove('slider__button--active');
+
+    const activeBtn = document.querySelector(`[data-image-num="${num}"]`);
+    activeBtn.firstElementChild.classList.add('slider__button--active');
+}
