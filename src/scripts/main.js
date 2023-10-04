48dd22e0ebe2e0eeb2e91d2ce0ed805735a35570
dima.rogkov <dima.rogkov@cheitgroup.com>
2023-10-04 22:41:42 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
       element: document.querySelector('.loader'),
       timeReady: 1200,
     },
+    header: document.querySelector('.header'),
   };
 
   // ===============================
@@ -24,4 +25,31 @@ window.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => document.body.classList.add('body--ready'), timeReady);
   };
   loaderInit(state.loader);
+
+  // ===============================
+  // HEADER
+  // ===============================
+  const headerInit = (header) => {
+    if (header) {
+      const burgerBtn = header.querySelector('.header__burger');
+      const layer = header.querySelector('.header__layer');
+
+      // toggle menu
+      const toggleMenu = () => {
+        document.body.classList.toggle('body--lock');
+        header.classList.toggle('header--menu-open');
+      };
+
+      // close menu
+      const closeMenu = () => {
+        document.body.classList.remove('body--lock');
+        header.classList.remove('header--menu-open');
+      };
+
+      // events
+      burgerBtn.addEventListener('click', toggleMenu);
+      layer.addEventListener('click', closeMenu);
+    }
+  };
+  headerInit(state.header);
 });
