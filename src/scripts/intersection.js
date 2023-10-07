351df1c0d8726d692795d8d203829f1bed9e66e6
boytrend0108 <boytrend@gmail.com>
2023-10-07 11:51:46 +0300

    complete task

---
+++ src/scripts/intersection.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+const target = document.querySelector('.actions__bottom');
+
+const callback = function() {
+  // eslint-disable-next-line no-trailing-spaces
+  window.innerWidth < 768
+    ? target.style.bottom = '74px'
+    : target.style.bottom = '148px';
+};
+
+// eslint-disable-next-line no-undef
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(target);
