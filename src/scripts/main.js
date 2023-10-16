06c2864d46ad985554cf5536dd034737bf2c062f
Valeriia <valery.volikova1@gmail.com>
2023-10-16 12:51:32 +0300

    page without image-scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const form = document.querySelector('.follow__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  window.scrollTo({ top: 0 });
+  form.reset();
+});
