febedfe79668dde10a9403c3f62ccca81a08fc69
Stanislav Kukharuk <kukharuk.stan1slav@gmail.com>
2023-10-22 20:40:44 +0200

    beta ver 4.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const page = document.documentElement;
+
+const menuOn = document.querySelector('.icon--menu');
+const menuOff1 = document.querySelector('.icon--close');
+const menuOff2 = document.querySelector('.menu__list');
+
+menuOn.addEventListener('click', () => {
+  page.classList.add('no-scroll');
+});
+
+menuOff1.addEventListener('click', () => {
+  page.classList.remove('no-scroll');
+});
+
+menuOff2.addEventListener('click', () => {
+  page.classList.remove('no-scroll');
+});
