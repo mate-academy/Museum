9356273ea6f845c5de194472387a79ea2c990dc0
Dominika Karmel <dkarmel@op.pl>
2023-10-25 15:10:23 +0200

    aome problem with main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
 'use strict';
+
+class Slider {
+  constructor(query) {
+    this.slider = document.querySelector(query); // element slider
+    this.slides = [...this.slider.querySelectorAll('.slider-slide')];
+    this.time = null;
+
+    this.currentSlide = Math.max(0, this.slides.findIndex(el =>
+      el.classList.contains('is-active')));
+    this.createPagination();
+    this.setSlide(this.currentSlide);
+  }
+
+  createPagination() {
+    this.dots = document.createElement('div');
+    this.dots.classList.add('slider-pagination');
+
+    this.slides.forEach((el, i) => {
+      const btn = document.createElement('button');
+
+      btn.classList.add('slider-pagination-button');
+      btn.type = 'button';
+      btn.innerText = i + 1;
+      btn.addEventListener('click', () => this.setSlide(i));
+      this.dots.append(btn);
+    });
+
+    this.slider.append(this.dots);
+  }
+
+  setSlide(index) {
+    this.slides.forEach(slide => {
+      slide.classList.remove('is-active');
+    });
+
+    this.slides[index].classList.add('is-active');
+
+    const dots = [...this.dots.children];
+
+    dots.forEach(dot => dot.classList.remove('is-active'));
+    dots[index].classList.add('is-active');
+    this.currentSlide = index;
+
+    clearTimeout(this.time);
+    this.time = setTimeout(() => this.slideNext(), 2500);
+  }
+
+  slideNext() {
+    this.currentSlide++;
+
+    if (this.currentSlide > this.slides.length - 1) {
+      this.currentSlide = 0;
+    }
+    this.setSlide(this.currentSlide);
+  }
+}
+
+const slider = new Slider('#mySlider');
