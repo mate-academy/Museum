9550fcc306a483411fbfd909b9da078be3864a1b
Andriy Fesych <andriyhimself@gmail.com>
2023-10-31 02:18:26 +0200

    save_0.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let sidebar = document.body.querySelector('.sidebar');
+let burgerIcon = document.body.querySelector('.header__burgerIcon');
+let burgerIconImg = document.body.querySelector('.header__burgerIconImg');
+
+let headerTitle = document.body.querySelector('.header__title');
+
+document.body.addEventListener('click', (event) => {
+  console.log(headerTitle);
+
+  console.log('mouse click on ', event.target);
+
+  if (sidebar.classList.contains('active')) {
+    sidebar.classList.remove('active');
+  } else if ([burgerIcon, burgerIconImg, sidebar].includes(event.target)
+  && !sidebar.classList.contains('active')) {
+    console.log('clicked on burger');
+
+    sidebar.classList.toggle('active');
+  }
+});
+
+([burgerIcon, burgerIconImg, sidebar].includes(event.target))
+// (event.target === burgerIcon || event.target === sidebar)
