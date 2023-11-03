f3a1e124f040d6f4333a2c393c4f097b71991331
Marina Semipyatnya <epicentr50@gmail.com>
2023-11-03 19:14:00 +0200

    add corrections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,108 @@
-'use strict';
+// document.getElementById('slide__form').onclick = function(e) {
+//   document.getElementById('input__form').value = '';
+// };
+
+// const sliderContainer = document.querySelector('.slider-container');
+// const bullets = document.querySelectorAll('.bullet');
+
+// bullets.forEach((bullet, index) => {
+//   bullet.addEventListener('click', () => {
+//     const slideWidth = sliderContainer.offsetWidth;
+//     sliderContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+
+//     // Highlight the selected bullet
+//     bullets.forEach(b => b.classList.remove('active'));
+//     bullet.classList.add('active');
+//   });
+// });
+
+// let swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 6,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 16,
+//     },
+//     420: {
+//       slidesPerView: 1.2,
+//       spaceBetween: 16,
+//     },
+//     500: {
+//      slidesPerView: 1.4,
+//      spaceBetween: 16,
+//     },
+//     520: {
+//      slidesPerView: 1.5,
+//       spaceBetween: 16,
+//     },
+//      540: {
+//      slidesPerView: 1.55,
+//      spaceBetween: 16,
+//      },
+//     560: {
+//        slidesPerView: 1.6,
+//        spaceBetween: 16,
+//     },
+//     590: {
+//      slidesPerView: 1.7,
+//      spaceBetween: 16,
+//      },
+//      630: {
+//       slidesPerView: 1.8,
+//       spaceBetween: 16,
+//     },
+//      660: {
+//        slidesPerView: 1.9,
+//        spaceBetween: 16,
+//     },
+//      690: {
+//        slidesPerView: 2,
+//        spaceBetween: 20,
+//       },
+//     720: {
+//        slidesPerView: 2.14,
+//        spaceBetween: 20,
+//         },
+//     835: {
+//      slidesPerView: 2.15,
+//      spaceBetween: 20,
+//     },
+//     865: {
+//      slidesPerView: 2.16,
+//      spaceBetween: 20,
+//      },
+//     895: {
+//      slidesPerView: 2.18,
+//      spaceBetween: 20,
+//      },
+//     930: {
+//      slidesPerView: 2.20,
+//      spaceBetween: 20,
+//       },
+//     965: {
+//      slidesPerView: 2.25,
+//       spaceBetween: 20,
+//      },
+//     1000: {
+//       slidesPerView: 2.30,
+//       spaceBetween: 20,
+//      },
+//     1024: {
+//       slidesPerView: 2.4,
+//      spaceBetween: 20,
+//      },
+//      1100: {
+//      slidesPerView: 2.5,
+//      spaceBetween: 20,
+//      },
+//     1160: {
+//       slidesPerView: 2.6,
+//       spaceBetween: 20,
+//     },
+//   },
+// });
