386517e7a4d56b289214cf3e2ca352a1f9b22415
Dominika Karmel <dkarmel@op.pl>
2023-11-10 13:32:16 +0100

    trying to solve gallery layout for desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,54 @@
 'use strict';
 
-// init Swiper:
+(function() {
+  'use strict';
 
-const swiper = new Swiper('.swiper', {
+  // breakpoint where swiper will be destroyed
+  // and switches to a dual-column layout
+  const breakpoint = window.matchMedia('(min-width:1280px)');
+
+  // keep track of swiper instances to destroy later
+  let mySwiper;
+
+  const breakpointChecker = function() {
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+      // clean up old instances and inline styles when available
+      if (mySwiper !== undefined) {
+        mySwiper.destroy(true, true);
+      }
+      // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+      // fire small viewport version of swiper
+      return enableSwiper();
+    }
+  };
+
+  const enableSwiper = function() {
+    mySwiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      a11y: true,
+      keyboardControl: true,
+      grabCursor: true,
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      autoplay: {
+        delay: 2000,
+      },
+    });
+  };
+
+  // keep an eye on viewport size changes
+  breakpoint.addListener(breakpointChecker);
+
+  // kickstart
+  breakpointChecker();
+})(); /* IIFE end */
+
+/**
+ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -16,6 +62,7 @@ const swiper = new Swiper('.swiper', {
     delay: 2000,
   },
 });
+ */
 
 // menu open
 
@@ -37,3 +84,14 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener('click', toggleMenu);
+
+// upward
+const scroll = document.querySelector('.upward');
+
+window.addEventListener('scroll', () => {
+  scroll.classList.toggle('upward--active', window.scrollY > 500);
+});
+
+scroll.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
