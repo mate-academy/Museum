0d9612cf138856cbbbf502a29fdf4017370a1d4a
Andriy Fesych <andriyhimself@gmail.com>
2023-11-11 23:14:09 +0200

    save_05

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,68 @@ let burgerIconImg = document.body.querySelector('.header__burgerIconImg');
 
 let headerTitle = document.body.querySelector('.header__title');
 
-document.body.addEventListener('click', (event) => {
-  console.log(headerTitle);
+// ([burgerIcon, burgerIconImg, sidebar].includes(event.target))
+// (event.target === burgerIcon || event.target === sidebar)
 
+let slideIndex = 1;
+let timer;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+/* function plusSlides(n) {
+  showSlides(slideIndex += n);
+} */
+
+// Thumbnail image controls
+let currentSlide = (n) => {
+  showSlides(n);
+};
+
+function showSlides(n) {
+  slideIndex = n;
+
+  let i;
+  const slides = document.querySelectorAll('.mySlides');
+  const dots = document.querySelectorAll('.dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+
+  // ADDED FOR IPAD
+  if ((window.innerWidth > 690) && (slideIndex < slides.length)) {
+    slides[slideIndex].style.display = 'block';
+  }
+  // REMOVE ABOVE IF NECESSARY
+
+  dots[slideIndex - 1].classList.add('active');
+
+  clearTimeout(timer);
+  timer = setTimeout(() => currentSlide(slideIndex + 1), 5000);
+}
+
+document.body.addEventListener('click', (event) => {
   console.log('mouse click on ', event.target);
+  console.log(`current slide index ${slideIndex}`);
+  const width = window.innerWidth;
+const height = window.innerHeight;
+
+console.log(`The viewport's width is ${width} and the height is ${height}.`);
 
   if (sidebar.classList.contains('active')) {
     sidebar.classList.remove('active');
@@ -19,7 +77,24 @@ document.body.addEventListener('click', (event) => {
 
     sidebar.classList.toggle('active');
   }
-});
 
-([burgerIcon, burgerIconImg, sidebar].includes(event.target))
-// (event.target === burgerIcon || event.target === sidebar)
+  if (event.target.classList.contains('dot')) {
+    console.log('clicked on dot');
+
+    if (event.target.classList.contains('dot1')) {
+      currentSlide(1);
+    }
+
+    if (event.target.classList.contains('dot2')) {
+      currentSlide(2);
+    }
+
+    if (event.target.classList.contains('dot3')) {
+      currentSlide(3);
+    }
+
+    if (event.target.classList.contains('dot4')) {
+      currentSlide(4);
+    }
+  }
+});
