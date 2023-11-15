f2617e094c1d5847c06b7ffc873742ca5aaf1526
Volodymyr Petrov <volodymyrpetrov2022@gmail.com>
2023-11-15 17:04:24 +0200

    Refactor according with suggested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,4 +13,5 @@ const form = document.querySelector('.contacts__form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   event.target.reset();
+  window.scrollTo(0, 0);
 });
