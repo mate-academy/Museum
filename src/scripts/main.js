df22a167540c83b318fa6cba22105d7171ac70c0
Denys209818 <deniskravcuk63@gmail.com>
2023-11-29 12:02:00 +0200

    Load museum. take1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,8 @@ document.getElementById('email-form').addEventListener('submit',
   function(event) {
     event.preventDefault();
     event.stopPropagation();
+
+    const input = document.getElementsByClassName('main__nav-email')[0];
+
+    input.value = '';
   });
