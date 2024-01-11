9369bc16fce2312de9c34fd26ca98ab0df255569
Illia <ilyaivasyk1@gmail.com>
2024-01-11 16:49:40 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,46 @@
-'use strict';
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('.center').slick({
+    autoplaySpeed: 0,
+    autoplay: false,
+    infinite: false,
+    centerMode: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          adaptiveHeight: true,
+          centerMode: true,
+          dots: true,
+          arrows: false,
+          draggable: false,
+          lazyLoad: 'eager',
+          centerPadding: '15px',
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          dots: true,
+          centerMode: true,
+          arrows: false,
+          draggable: false,
+          lazyLoad: 'eager',
+          centerPadding: '10px',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
