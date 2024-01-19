108786b5de2ffcce3a1b29348a28a66f9326f2da
Misha Remeslo <misha.remeslol@gmail.com>
2024-01-19 19:51:25 +0200

    landing Museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// eslint-disable-next-line no-unused-vars
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName('gallery-slide');
+  const dots = document.getElementsByClassName('gallery__dot');
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex - 1].style.display = 'inline-block';
+  slides[slideIndex].style.display = 'inline-block';
+  dots[slideIndex].className += ' active';
+}
