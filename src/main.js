d030b27111da7b1ed756788e46b8a6a02e6b6c9d
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2024-01-24 19:15:26 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,27 +1,49 @@
-'use strict';
+// 'use strict';
 
-let modal;
+// let modal;
 
+// function toggleModal(event) {
+//   event.stopPropagation();
+//   modal.classList.toggle('show');
+// }
+
+// function closeModal() {
+//   modal.classList.remove('show');
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   modal = document.getElementById('modalMenu');
+//   modal.addEventListener('click', closeModal);
+
+//   const menuOpener = document.querySelector('.menu__opener');
+
+//   menuOpener.addEventListener('click', toggleModal);
+// });
+
+// function sendForm() {
+//   const subscribeTextarea = document.getElementById('subscribe');
+
+//   subscribeTextarea.value = '';
+// };
 function toggleModal(event) {
   event.stopPropagation();
-  modal.classList.toggle('show');
-}
 
-function closeModal() {
-  modal.classList.remove('show');
+  const modal = document.getElementById('modalMenu');
+
+  modal.classList.toggle('show');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   modal = document.getElementById('modalMenu');
-  modal.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', () => {
+    modal.classList.remove('show');
+  });
 
   const menuOpener = document.querySelector('.menu__opener');
 
-  menuOpener.addEventListener('click', toggleModal);
+  menuOpener.addEventListener('click', (event) => {
+    event.stopPropagation();
+    modal.classList.toggle('show');
+  });
 });
-
-function sendForm() {
-  const subscribeTextarea = document.getElementById('subscribe');
-
-  subscribeTextarea.value = '';
-};
