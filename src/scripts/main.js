faad021e057c9b553193a41c44a9317a4fa00961
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2024-01-24 15:34:36 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let modal;
+
+function toggleModal(event) {
+  event.stopPropagation();
+  modal.classList.toggle('show');
+}
+
+function closeModal() {
+  modal.classList.remove('show');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  modal = document.getElementById('modalMenu');
+  modal.addEventListener('click', closeModal);
+
+  const menuOpener = document.querySelector('.menu__opener');
+
+  menuOpener.addEventListener('click', toggleModal);
+});
+
+function sendForm() {
+  const subscribeTextarea = document.getElementById('subscribe');
+
+  subscribeTextarea.value = '';
+};
