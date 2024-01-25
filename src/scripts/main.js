e16ad5223232b00398516dcd65006c28c8dff980
Maryna1313 <maryna.tarhonska@gmail.comcode>
2024-01-25 18:42:37 +0100

    add fix slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -35,3 +35,19 @@ function currentSlide(slideNumber) {
   }
 }
 currentSlide(1);
+
+document.querySelector('.gallery__dot--1').addEventListener('click', () => {
+  currentSlide(1);
+});
+
+document.querySelector('.gallery__dot--2').addEventListener('click', () => {
+  currentSlide(2);
+});
+
+document.querySelector('.gallery__dot--3').addEventListener('click', () => {
+  currentSlide(3);
+});
+
+document.querySelector('.gallery__dot--4').addEventListener('click', () => {
+  currentSlide(4);
+});
