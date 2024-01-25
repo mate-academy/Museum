e6e27b11938779282c475a3501c0729be066d7c7
Misha Siv <0Sivcov0@gmail.com>
2024-01-25 19:05:58 +0200

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-'use strict';
+window.addEventListener('hashchange', () => {
+  const body = document.body;
+
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+    body.style.overflow = 'hidden';
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+    body.style.overflow = '';
+  }
+});
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+  window.location.hash = '';
+});
