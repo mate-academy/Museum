2332b41b430841fb2a2d049e35168c036fd4228b
Misha Siv <0Sivcov0@gmail.com>
2024-02-03 14:04:45 +0200

    bug fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,7 @@
 window.addEventListener('hashchange', () => {
-  const body = document.body;
-
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
-    body.style.overflow = 'hidden';
   } else {
     document.body.classList.remove('page__body--with-menu');
-    body.style.overflow = '';
   }
-});
-
-const button = document.querySelector('#button');
-
-button.addEventListener('click', () => {
-  window.location.hash = '';
 });
