cf2583ea1af95d0bfa183aaeeea2e831bace4179
Yevhen Podobedo <yevhen.podobedo@gmail.com>
2024-02-06 18:51:20 +0200

    added task layout Museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-'use strict';
+// duplicate tags
+const sourceContentOne = document.getElementById('work-week').innerHTML;
+const duplicationContentOne = document.getElementById('duplication-work-week');
+
+duplicationContentOne.innerHTML = sourceContentOne;
+
+const sourceContentTwo = document.getElementById('menu').innerHTML;
+const duplicateContentTwo = document.getElementById('duplication-menu');
+
+duplicateContentTwo.innerHTML = sourceContentTwo;
+
+// stop page
+const page = document.querySelector('.page');
+const locking = document.querySelector('.header__burger-menu');
+
+locking.addEventListener('click', function() {
+  page.classList.add('page__lock');
+});
+
+const unlockingClose = document.querySelector('.burger-menu__close');
+const unlockingMenu = document.querySelector('.menu__list');
+
+unlockingClose.addEventListener('click', function() {
+  page.classList.remove('page__lock');
+});
+
+unlockingMenu.addEventListener('click', function() {
+  page.classList.remove('page__lock');
+});
