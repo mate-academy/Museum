17a9b724404d8704bc40092faf9cefea0085aeaa
Misha Remeslo <misha.remeslol@gmail.com>
2024-02-16 22:02:58 +0200

    landing museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,43 @@ let slideIndex = 1;
 
 showSlides(slideIndex);
 
-// eslint-disable-next-line no-unused-vars
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName('gallery-slide');
   const dots = document.getElementsByClassName('gallery__dot');
 
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
 
-  slides[slideIndex - 1].style.display = 'inline-block';
-  slides[slideIndex].style.display = 'inline-block';
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = slides[i].style.display.replace('active', 'none');
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  slides[slideIndex].style.display = 'block';
+  slides[slideIndex - 1].style.display = 'block';
+
   dots[slideIndex].className += ' active';
+}
+
+// eslint-disable-next-line no-unused-vars
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// eslint-disable-next-line no-unused-vars
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
