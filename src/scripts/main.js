7fb501d09e6c1a2e366a849270c62d8dad591bdb
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-02-21 20:51:43 +0200

    shadowed menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const openMenu = document.querySelector('.icon--menu');
+const closeMenu = document.querySelector('.icon--close');
+
+openMenu.addEventListener('click', function() {
+  document.querySelector('.page').classList.toggle('target-active');
+});
+
+closeMenu.addEventListener('click', function() {
+  document.querySelector('.page').classList.toggle('target-active');
+});
