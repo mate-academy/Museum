605b450d3dd8eb930a54bb319b9116afa013d3a3
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-02-22 22:33:11 +0200

    land site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,14 @@ openMenu.addEventListener('click', function() {
 closeMenu.addEventListener('click', function() {
   document.querySelector('.page').classList.toggle('target-active');
 });
+
+let counter = 1;
+
+setInterval(function() {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
