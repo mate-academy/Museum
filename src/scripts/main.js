0b079c5d2e3a9fbb9dad3ede1c05d9f7d6fbe0f3
Viktor <victor.tsupa@gmail.com>
2024-02-26 19:11:11 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-'use strict';
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.icon--burger')) {
+    document.body.style.overflow = 'hidden';
+  } else if (event.target.matches('.icon--close,.menu__link')) {
+    document.body.style.overflow = 'auto';
+  }
+});
