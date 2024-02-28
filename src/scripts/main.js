f93a85b9af6f31d5c746f613f825d809bada4204
Yehor <eee.houmich@gmail.com>
2024-02-28 14:22:13 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  width: 280,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      width: 340,
+    },
+  },
+});
+
+const link = document.getElementById('link');
+
+if (window.screen.availWidth >= 1280) {
+  link.href = '#gallery';
+}
