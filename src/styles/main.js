4fe13ea1e25589b638849d017c4c15470b8ad754
Denis Kotov <dionisiy2904@gmail.com>
2024-03-04 17:59:44 +0200

    LandingMuseum fix and changing size

--- src/styles/main.js
+++ src/styles/main.js
@@ -3,43 +3,32 @@ const slider = document.querySelector('.swiper');
 let swiper;
 
 function modileSlider() {
-  if (window.innerWidth > 672
-    && window.innerWidth <= 1280
+  if (window.innerWidth <= 1024
     && slider.dataset.slider === 'false') {
     // eslint-disable-next-line no-undef
     swiper = new Swiper('.swiper', {
-      centeredSlides: true,
-      slidesPerView: 4,
-
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      spaceBetween: 10,
-
-    });
-
-    slider.dataset.slider = 'true';
-  }
-
-  if (window.innerWidth <= 672 && slider.dataset.slider === 'false') {
-    // eslint-disable-next-line no-undef
-    swiper = new Swiper('.swiper', {
-      centeredSlides: true,
       slidesPerView: 1,
-
+      spaceBetween: 15,
+      centeredSlides: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
-      spaceBetween: 5,
+      width: 280,
 
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          width: 340,
+        },
+      },
     });
 
     slider.dataset.slider = 'true';
   }
 
-  if (window.innerWidth > 1280) {
+  if (window.innerWidth > 1024) {
     slider.dataset.slider = 'false';
 
     if (slider.classList.contains('swiper-initialized')) {
