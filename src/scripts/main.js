8f689bbe8c0b68f85659670cef268d09bf4156a9
Seva Podolskiy <s_podolskiy@live.com>
2024-03-29 09:20:03 -0400

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  // centeredSlides: true,
+  spaceBetween: 20,
+  slidesPerView: 'auto',
+  effect: 'slide',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
