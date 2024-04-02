e70ea505034b7adcc664874eb91a77bfdf2529f1
Seva Podolskiy <s_podolskiy@live.com>
2024-04-02 12:53:35 -0400

    fix burger menu not hiding when clicked to open menue JS implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,3 +23,35 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+/* #region hide menu icon on click */
+const menuIcon = document.querySelector('.page__menu-icon-img');
+const closeIcon = document.querySelector('.menu__close-link');
+// const icons = document.querySelector('.top-bar__icons');
+// const logoTopBar = document.querySelector('.menu .top-bar__logo-img');
+const menuLinks = document.querySelectorAll(
+  '.menu__nav-list .menu__nav-list-item',
+);
+
+console.log(menuIcon);
+console.log(closeIcon);
+console.log(menuLinks);
+
+menuIcon?.addEventListener('click', () => {
+  menuIcon.classList.toggle('page__menu-icon-img--hide-show');
+});
+
+closeIcon?.addEventListener('click', () => {
+  menuIcon.classList.toggle('page__menu-icon-img--hide-show');
+});
+
+// logoTopBar?.addEventListener('click', () => {
+//   icons.classList.toggle('top-bar__icons--show');
+// });
+
+menuLinks?.forEach((node) => {
+  node?.addEventListener('click', () => {
+    menuIcon.classList.toggle('page__menu-icon-img--hide-show');
+  });
+});
+/* #end hide menu icon on click */
