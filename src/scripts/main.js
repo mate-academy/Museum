cff1c3c3ed5511d68432c15c622030acf3d84b56
HunchakAndrii <andreygunchak1@gmail.com>
2024-04-03 12:41:01 +0300

    q & a

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-'use strict';
+// 'use strict';
+
+
+// const burger = document.querySelector('.burger');
+// const mobileMenu = document.querySelector('.mobile-menu');
+// const html = document.querySelector('html');
+
+// burger.addEventListener('click', (event) => {
+//   event.preventDefault();
+
+//   burger.classList.toggle('active');
+//   mobileMenu.classList.toggle('active');
+//   html.classList.toggle('active');
+// })
+
+
+// mobileMenu.addEventListener('transitionend', function () {
+//   if (window.location.hash === '#mobile-menu') {
+//     this.scrollTop = 0;
+//   }
+// });
