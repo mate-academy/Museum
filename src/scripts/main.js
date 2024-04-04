9b0d2c8ecc4f5b154f28cbec0fc966bacf8aa63c
Vadim <volandemortuwka@gmail.com>
2024-04-04 14:46:34 +0300

    save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  const closeBtn = document.querySelector('.nav__close');
+  const navMenu = document.querySelector('.nav');
+
+  closeBtn.addEventListener('click', function() {
+    navMenu.classList.remove('nav--open');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const headerBtn = document.querySelector('.header__btn');
+  const navMenu = document.querySelector('.nav');
+
+  headerBtn.addEventListener('click', function() {
+    navMenu.classList.toggle('nav--open');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const galleryWrapper = document.querySelector('.gallery__wrapper');
+  const galleryImages = document.querySelector('.gallery__gallery');
+  const galleryButtons = galleryWrapper.querySelectorAll('.gallery__circle');
+
+  galleryButtons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      const containerWidth = galleryImages.offsetWidth;
+
+      galleryImages.style.transition = 'transform 0.3s ease-in-out';
+
+      galleryImages.style.transform = 'translateX(-'
+        + +(containerWidth * index) + 'px)';
+    });
+  });
+});
+
+const emailInput = document.getElementById('emailInput');
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  emailInput.value = '';
+});
