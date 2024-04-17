4625135ce34cc20f844dcdf00b1bb991a7cd1d51
HunchakAndrii <andreygunchak1@gmail.com>
2024-04-17 17:12:41 +0300

    fixed mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,71 +1,46 @@
 // 'use strict';
 import Swiper from 'swiper';
 
-
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const html = document.querySelector('html');
 const darkBackground = document.querySelector('.dark-background');
 const navLink = document.querySelectorAll('.nav__link');
 
-burger.addEventListener('click', (event) => {
-  event.preventDefault();
-
+const toggleMobileMenu = () => {
   burger.classList.toggle('active');
   mobileMenu.classList.toggle('active');
   html.classList.toggle('active');
   darkBackground.classList.toggle('active');
-})
+};
 
-navLink.forEach(navLink => {
+const removeMobileMenu = () => {
+  burger.classList.remove('active');
+  mobileMenu.classList.remove('active');
+  html.classList.remove('active');
+  darkBackground.classList.remove('active');
+};
+
+burger.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  toggleMobileMenu();
+});
+
+navLink.forEach((navLink) => {
   navLink.addEventListener('click', (event) => {
-
-    burger.classList.remove('active');
-    mobileMenu.classList.remove('active');
-    html.classList.remove('active');
-    darkBackground.classList.remove('active');
+    removeMobileMenu();
   });
-})
+});
 
+darkBackground.addEventListener('click', (event) => {
+  event.preventDefault();
 
+  removeMobileMenu();
+});
 
-
-
-  //  var getScrollBarSize = (function () {
-  //    var el = window.document.createElement('textarea'),
-  //      style = {
-  //        visibility: 'hidden',
-  //        position: 'absolute',
-  //        zIndex: '-2147483647',
-  //        top: '-1000px',
-  //        left: '-1000px',
-  //        width: '1000px',
-  //        height: '1000px',
-  //        overflow: 'scroll',
-  //        margin: '0',
-  //        border: '0',
-  //        padding: '0',
-  //      },
-  //      support = el.clientWidth !== undefined && el.offsetWidth !== undefined;
-
-  //    for (var key in style) {
-  //      if (style.hasOwnProperty(key)) {
-  //        el.style[key] = style[key];
-  //      }
-  //    }
-
-  //    return function () {
-  //      var size = null;
-  //      if (support && window.document.body) {
-  //        window.document.body.appendChild(el);
-  //        size = [
-  //          el.offsetWidth - el.clientWidth,
-  //          el.offsetHeight - el.clientHeight,
-  //        ];
-  //        window.document.body.removeChild(el);
-  //      }
-  //      return size;
-  //    };
-  //  })();
-
-  //  console.log(getScrollBarSize());
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    removeMobileMenu();
+  }
+});
