f3c615badafb95cc447a7411e416bdcc047da895
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-05-26 20:19:46 +0300

    add task solution

--- src/script.js
+++ src/script.js
@@ -1,22 +1,20 @@
-'use strict';
-
 let startX;
 let endX;
 
-const slider = document.querySelector('.slider');
+const slider = document.querySelector('.clider');
 
 slider.addEventListener('touchstart', (e) => {
-  startX = e.changedTouches[0].clientX;
-  console.log(startX);
+  startX = e.changeTouches[0].clientX;
 });
 
 slider.addEventListener('touchend', (e) => {
-  endX = e.changedTouches[0].clientX;
+  endX = e.changeTouches[0].clientX;
   handleSwipe();
 });
 
 function handleSwipe() {
   if (startX - endX > 100) {
+    // Swipe to the left
     const nextSlide = document.querySelector(
       'input[name="slide"]:checked',
     ).nextElementSibling;
@@ -24,6 +22,7 @@ function handleSwipe() {
       nextSlide.checked = true;
     }
   } else if (startX - endX < -100) {
+    // Swipe to the right
     const prevSlide = document.querySelector(
       'input[name="slide"]:checked',
     ).previousElementSibling;
