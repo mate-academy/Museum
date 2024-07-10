bd691124a0461ed84f9441407df386a1031c0b26
user <mariiaantoniv@gmail.com>
2024-07-10 14:52:34 +0300

    museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+var imgSwiper = new Swiper('.slide-content',{
+  slidesPerView: 1,
+  spaceBetween: 15,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    deplay: 2000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  }
+});
