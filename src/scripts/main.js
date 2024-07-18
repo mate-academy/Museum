7e7d69d4ce8c663a086933725e783f48d925e881
Tanya Baletska <t.baletska@gmail.com>
2024-07-18 19:07:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+  const indicators = document.querySelectorAll(".gallery__indicator");
+
+  function goToSlide(slide) {
+    const galleryList = document.querySelector(".gallery__list");
+
+    galleryList.style.transform = `translateX(-${slide * 40}%)`;
+    indicators.forEach((indicator, index) => {
+      indicator.classList.toggle("active", index === slide);
+    });
+  }
+
+  indicators.forEach((indicator, index) => {
+    indicator.addEventListener("click", () => {
+      goToSlide(index);
+    });
+  });
+
+  window.addEventListener("resize", () => {
+    const activeIndicator = document.querySelector(".gallery__indicator.active");
+    if (activeIndicator) {
+      goToSlide(Array.from(indicators).indexOf(activeIndicator));
+    }
+  });
+
+  goToSlide(0);
+});
+
+
