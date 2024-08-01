b00c474f3a203e831c6beb8803bda416c54a0b23
mrsvolodya <vovamursky@gmail.com>
2024-08-01 17:15:51 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+const btnContainer = document.querySelector('.gallery__controls-list');
+
+function isScroll(event) {
+  const isScroll = event.target.href?.slice(-4) === 'menu';
+
+  if (isScroll) {
+    document.documentElement.style.cssText = `
+    position: fixed;
+    top: 0;
+    inline-size: 100%;
+    overflow-y:scroll;
+    `;
+  } else {
+    document.documentElement.style.cssText = ``;
+  }
+}
+
+function isMove(event) {
+  const imgContainer = document.querySelector('.gallery__container-list');
+  const imgs = document.querySelectorAll('.gallery__image');
+  const rectPrev = imgs[0].getBoundingClientRect();
+  const rectNext = imgs[1].getBoundingClientRect();
+  const spaceBetweenImg = (rectNext.right - rectNext.width * 2 - rectPrev.left);
+  const pureWidth = rectPrev.width + spaceBetweenImg;
+
+  if (event.target.closest('.gallery__controls-btn')) {
+    const btnIndex = +event.target.dataset.index;
+
+    imgContainer.style.cssText = `
+    transform: translateX(-${pureWidth * btnIndex}px);
+    transition: all .3s ease-in-out;
+    `;
+  }
+}
+
+btnContainer.addEventListener('click', isMove);
+document.addEventListener('click', isScroll);
