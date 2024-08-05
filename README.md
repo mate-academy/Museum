29723ab96a1044735ba6e16e60d039c545d77b09
mrsvolodya <vovamursky@gmail.com>
2024-08-05 12:24:13 +0300

    some-improve

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ Check font styles. Use [IBM Plex Sans](https://fonts.google.com/specimen/IBM+Ple
 1. Implement footer.
 ## Checklist for preparing a portfolio project for HR review
 
-1. Don’t forget to add a title for the whole web page (it could be the name of your landing)
++1. Don’t forget to add a title for the whole web page (it could be the name of your landing)
 2. A landing page is implemented strictly according to the design in Figma
-4. Links in the header and footer menus should lead to the corresponding blocks of the landing page
++4. Links in the header and footer menus should lead to the corresponding blocks of the landing page
 5. The speed of animations is the same throughout the landing page (for example, increasing when hovering or moving blocks when scrolling)
 6. Placeholders in the forms suggest what to enter, and if there is a validation of the form, then it is clear in what format to enter the phone number
 7. Make sure everything looks neat on mobile and without horizontal scrolling
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,144 @@
 'use strict';
 const btnContainer = document.querySelector('.gallery__controls-list');
+const btns = document.querySelectorAll('.gallery__controls-btn');
+const imgContainer = document.querySelector('.gallery__container-list');
+const toTopBtn = document.querySelector('.footer__scroll-to-top');
+const submitBtn = document.querySelector('.subscribe__submit-btn');
+const ticketBtn = document.querySelector('.header__t-button');
+const form = document.querySelector('.subscribe__form');
 
-function isScroll(event) {
-  const isScroll = event.target.href.slice(-4) === 'menu';
+let btnIndex = 0;
+let startX = 0;
+let endX = 0;
+let isMouseDown = false;
 
-  if (isScroll) {
-    document.documentElement.style.cssText = `
-    position: fixed;
-    top: 0;
-    inline-size: 100%;
-    overflow-y:scroll;
-    `;
-  } else {
-    document.documentElement.style.cssText = ``;
-  }
-}
-
-function isMove(event) {
-  const imgContainer = document.querySelector('.gallery__container-list');
+function computedMove(index) {
   const imgs = document.querySelectorAll('.gallery__image');
   const rectPrev = imgs[0].getBoundingClientRect();
   const rectNext = imgs[1].getBoundingClientRect();
-  const spaceBetweenImg = (rectNext.right - rectNext.width * 2 - rectPrev.left);
+  const spaceBetweenImg = rectNext.right - rectNext.width * 2 - rectPrev.left;
   const pureWidth = rectPrev.width + spaceBetweenImg;
 
-  if (event.target.closest('.gallery__controls-btn')) {
-    const btnIndex = +event.target.dataset.index;
+  btns.forEach((btn) => {
+    btn.classList.remove('gallery__active-btn');
+  });
 
-    imgContainer.style.cssText = `
-    transform: translateX(-${pureWidth * btnIndex}px);
-    transition: all .3s ease-in-out;
-    `;
+  const activeBtn = Array.from(btns).find(
+    (btn) => +btn.dataset.index === +index,
+  );
+
+  if (activeBtn) {
+    activeBtn.classList.add('gallery__active-btn');
+  }
+
+  imgContainer.style.transform = `translateX(-${pureWidth * index}px)`;
+}
+
+function isMove(event) {
+  const element = event.target;
+
+  if (element.closest('.gallery__controls-btn')) {
+    btnIndex = +element.dataset.index;
+    computedMove(btnIndex);
   }
 }
 
+function isActive() {
+  btns[0].classList.add('gallery__active-btn');
+
+  function isClick(e) {
+    const element = e.target;
+
+    if (element.closest('.gallery__controls-btn')) {
+      btns.forEach((btn) => {
+        btn.classList.remove('gallery__active-btn');
+        element.classList.add('gallery__active-btn');
+      });
+    }
+  }
+
+  btnContainer.addEventListener('click', isClick);
+}
+
+imgContainer.addEventListener('mousedown', function (e) {
+  startX = e.clientX;
+  isMouseDown = true;
+});
+
+imgContainer.addEventListener('touchstart', function (e) {
+  startX = e.touches[0].clientX;
+  isMouseDown = true;
+});
+
+document.addEventListener('mouseup', (e) => {
+  if (isMouseDown) {
+    endX = e.clientX;
+    isMouseDown = false;
+
+    const distanceX = endX - startX;
+
+    if (distanceX > 0 && btnIndex > 0) {
+      btnIndex--;
+      computedMove(btnIndex);
+    } else if (distanceX < 0 && btnIndex < btns.length - 1) {
+      btnIndex++;
+      computedMove(btnIndex);
+    }
+  }
+});
+
+document.addEventListener('touchend', (e) => {
+  if (isMouseDown) {
+    endX = e.changedTouches[0].clientX;
+    isMouseDown = false;
+    const distanceX = endX - startX;
+
+    if (distanceX > 0 && btnIndex > 0) {
+      btnIndex--;
+      computedMove(btnIndex);
+    } else if (distanceX < 0 && btnIndex < btns.length - 1) {
+      btnIndex++;
+      computedMove(btnIndex);
+    }
+  }
+});
+
+function scrollTo() {
+  window.scroll(0, 0);
+}
+
+function onSubmit(event) {
+  const field = form.querySelector('.subscribe__field');
+  if (field.value) {
+    event.preventDefault();
+    form.reset();
+    window.scroll(0, 0);
+  }
+}
+
+toTopBtn.addEventListener('click', scrollTo);
+document.addEventListener('DOMContentLoaded', isActive, { once: true });
 btnContainer.addEventListener('click', isMove);
-document.addEventListener('click', isScroll);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.icon--menu');
+  const closeBtn = document.querySelector('.icon--close');
+  const menuNav = document.querySelector('.menu__nav');
+
+  menuBtn.addEventListener('click', () => {
+    document.documentElement.classList.add('menu-open');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    document.documentElement.classList.remove('menu-open');
+  });
+
+  menuNav.addEventListener('click', () => {
+    document.documentElement.classList.remove('menu-open');
+  });
+});
+
+submitBtn.addEventListener('click', onSubmit);
+ticketBtn.addEventListener('click', () => {
+  document.querySelector('.performances').scrollIntoView();
+});
