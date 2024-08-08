889dddc28c4ad418f30a7eec087bf61916e672c0
Bohdan Ivankov <bodanich600@gmail.com>
2024-08-08 12:22:41 +0100

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sliderList = document.querySelector('.slider__list');
+  const buttons = document.querySelectorAll('.slider__buttons-btn');
+  const itemWidth = sliderList.querySelector('.slider__item').offsetWidth;
+
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      sliderList.style.transform = `translateX(-${index * itemWidth}px)`;
+
+      buttons.forEach((btn) =>
+        btn.classList.remove('slider__buttons-btn--active'));
+      button.classList.add('slider__buttons-btn--active');
+    });
+  });
+});
