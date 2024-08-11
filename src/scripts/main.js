34f6711f985f496d6d54d9295dad8b0a4eb02cc3
Bohdan Ivankov <bodanich600@gmail.com>
2024-08-11 13:43:13 +0100

    make some fixes in scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelector('.slider');
-  const sliderList = document.querySelector('.slider__list');
-  const slides = document.querySelectorAll('.slider__item');
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.querySelector(".slider");
+  const sliderList = document.querySelector(".slider__list");
+  const slides = document.querySelectorAll(".slider__item");
 
   let currentIndex = 0;
 
@@ -17,13 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function initializeSettings() {
     if (window.innerWidth >= 1280) {
-      const buttons = document.querySelector('.slider__buttons');
+      const buttons = document.querySelector(".slider__buttons");
 
       if (buttons) {
         buttons.remove();
       }
-      slider.removeAttribute('style');
-      sliderList.removeAttribute('style');
+      slider.removeAttribute("style");
+      sliderList.removeAttribute("style");
     } else if (window.innerWidth >= 768 && window.innerWidth <= 1280) {
       settings.itemWidth = 340;
       settings.frameSize = 2;
@@ -32,14 +32,15 @@ document.addEventListener('DOMContentLoaded', () => {
       settings.previewWidth = 14;
 
       slider.style.minWidth = `${
-        settings.itemWidth * settings.frameSize
-        + settings.gap + settings.previewWidth
+        settings.itemWidth * settings.frameSize +
+        settings.gap +
+        settings.previewWidth
       }px`;
 
       const buttons = updateButtons();
 
       handleButtonsClick(buttons);
-      sliderList.style.transform = 'translateX(0)';
+      sliderList.style.transform = "translateX(0)";
     } else {
       settings.itemWidth = 280;
       settings.frameSize = 1;
@@ -48,24 +49,25 @@ document.addEventListener('DOMContentLoaded', () => {
       settings.previewWidth = 4;
 
       slider.style.minWidth = `${
-        settings.itemWidth * settings.frameSize
-        + settings.gap + settings.previewWidth
+        settings.itemWidth * settings.frameSize +
+        settings.gap +
+        settings.previewWidth
       }px`;
 
       const buttons = updateButtons();
 
       handleButtonsClick(buttons);
 
-      sliderList.style.transform = 'translateX(0)';
+      sliderList.style.transform = "translateX(0)";
     }
   }
 
   function updateButtons() {
-    const buttonsСontainer = document.createElement('ul');
+    const buttonsСontainer = document.createElement("ul");
 
-    buttonsСontainer.classList.add('slider__buttons');
+    buttonsСontainer.classList.add("slider__buttons");
 
-    const existingButtons = document.querySelector('.slider__buttons');
+    const existingButtons = document.querySelector(".slider__buttons");
 
     if (existingButtons) {
       existingButtons.remove();
@@ -80,22 +82,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const numButtons = Math.ceil((totalSlides - frameSize) / step) + 1;
 
     for (let i = 0; i < numButtons; i++) {
-      const item = document.createElement('li');
-      const button = document.createElement('button');
+      const item = document.createElement("li");
+      const button = document.createElement("button");
 
-      item.classList.add('slider__buttons-item');
-      button.classList.add('slider__buttons-btn');
+      item.classList.add("slider__buttons-item");
+      button.classList.add("slider__buttons-btn");
 
       item.appendChild(button);
       buttonsСontainer.appendChild(item);
     }
 
-    return buttonsСontainer.querySelectorAll('.slider__buttons-btn');
+    return buttonsСontainer.querySelectorAll(".slider__buttons-btn");
   }
 
   function handleButtonsClick(currentButtons) {
     currentButtons.forEach((button, index) => {
-      button.addEventListener('click', () => {
+      button.addEventListener("click", () => {
         if (index > currentIndex) {
           currentIndex = index * settings.step;
         } else {
@@ -103,12 +105,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         currentButtons.forEach((btn) =>
-          btn.classList.remove('slider__buttons-btn--active'));
-        button.classList.add('slider__buttons-btn--active');
+          btn.classList.remove("slider__buttons-btn--active")
+        );
+        button.classList.add("slider__buttons-btn--active");
 
         const sliderWidth = slider.offsetWidth;
-        const totalWidth = slides.length * (settings.itemWidth + settings.gap)
-          - settings.gap;
+        const totalWidth =
+          slides.length * (settings.itemWidth + settings.gap) - settings.gap;
 
         let offset;
 
@@ -117,20 +120,21 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (currentIndex >= slides.length - settings.frameSize) {
           offset = totalWidth - sliderWidth;
         } else {
-          offset = currentIndex * (settings.itemWidth + settings.gap)
-            - (sliderWidth - settings.itemWidth) / 2
-            + settings.previewWidth / 2;
+          offset =
+            currentIndex * (settings.itemWidth + settings.gap) -
+            (sliderWidth - settings.itemWidth) / 2 +
+            settings.previewWidth / 2;
         }
 
         sliderList.style.transform = `translateX(-${offset}px)`;
       });
     });
 
-    currentButtons[0].classList.add('slider__buttons-btn--active');
+    currentButtons[0].classList.add("slider__buttons-btn--active");
   }
 
   initializeSettings();
-  window.addEventListener('resize', initializeSettings);
+  window.addEventListener("resize", initializeSettings);
 
   // Touch
   let isDragging = false;
@@ -138,9 +142,9 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentTranslate = 0;
   let prevTranslate = 0;
 
-  slider.addEventListener('touchstart', touchStart);
-  slider.addEventListener('touchmove', touchMove);
-  slider.addEventListener('touchend', touchEnd);
+  slider.addEventListener("touchstart", touchStart);
+  slider.addEventListener("touchmove", touchMove);
+  slider.addEventListener("touchend", touchEnd);
 
   function touchStart(event) {
     isDragging = true;
@@ -162,14 +166,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const movedBy = startPos - e.changedTouches[0].clientX;
 
-    if (movedBy > 50 && currentIndex < slides.length - settings.frameSize) {
-      currentIndex += settings.step;
-    } else if (movedBy < -50 && currentIndex > 0) {
-      currentIndex -= settings.step;
+    if (Math.abs(movedBy) > 50) {
+      if (movedBy > 50 && currentIndex < slides.length - settings.frameSize) {
+        currentIndex += settings.step;
+      } else if (movedBy < -50 && currentIndex > 0) {
+        currentIndex -= settings.step;
+      }
     }
 
     // Використання вашої логіки перемикання
-    const buttons = document.querySelectorAll('.slider__buttons-btn');
+    const buttons = document.querySelectorAll(".slider__buttons-btn");
 
     buttons[currentIndex / settings.step].click();
   }
