242fa03731681ce0e18c775a5fe0941ca0eec6c5
Yana <anazigaluk@gmail.com>
2024-10-27 00:20:46 +0300

    create museum landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const overlay = document.querySelector('.page__overlay');
+const activateOverlay = document.querySelector('.museum__icon');
+const activationOverlay = document.querySelector('.header__icon');
+const disableOverlay = document.querySelector('.menu__icon');
+const body = document.querySelector('body');
+activateOverlay.addEventListener('click', () => {
+overlay.style.opacity = 1;
+overlay.style.pointerEvents = "auto";
+body.style.height = "100%";
+body.style.overflowY = "hidden";
+});
+activationOverlay.addEventListener('click', () => {
+overlay.style.opacity = 1;
+overlay.style.pointerEvents = "auto";
+body.style.height = "100%";
+body.style.overflowY = "hidden";
+});
+disableOverlay.addEventListener('click', () => {
+overlay.style.opacity = 0;
+overlay.style.pointerEvents = "none";
+body.style.height = "unset";
+body.style.overflowY = "auto";
+});
