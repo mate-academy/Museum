e08bc83a1615ab46784fc3777a29cea819700f7b
Iryna <irakogut23190@gmail.com>
2024-10-30 22:18:07 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,22 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    920: {
+      slidesPerView: 3,
+    },
+  },
   spaceBetween: 30,
-  loop: true,
+  loop: false,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    dynamicBullets: true,
-    clickable: true,
   },
 });
