8c8da1c2abbe1b89c006818ac3c6ed09b230ed7a
Lidiia Shvets <lepoki051003@gmail.com>
2024-11-08 18:04:01 +0100

    Museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
+/* eslint-disable no-undef */
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  // eslint-disable-next-line no-unused-vars
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 16,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButton = document.querySelector('.icon--menu');
+  const closeButton = document.querySelector('.icon--close');
+  const body = document.querySelector('body');
+
+  menuButton.addEventListener('click', () => {
+    body.classList.add('menu-open');
+  });
+
+  closeButton.addEventListener('click', () => {
+    body.classList.remove('menu-open');
+  });
+});
