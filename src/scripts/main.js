27478858e939c2a7636eb2a926c6be400cd8715c
beliyandr <beliyandr@gmail.com>
2024-11-12 08:39:38 +0200

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const menu = document.querySelector('.header__menu');
+const menuBurger = document.querySelector('.header__burder');
+const menuClose = document.querySelector('.menu__close');
+const body = document.querySelector('.wrapper');
+
+function activeMenu() {
+  body.classList.add('active');
+  // body.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+  // body.style.opacity = 0.1;
+}
+function closeMenu() {
+  body.classList.remove('active');
+  // body.style.backgroundColor = 'inherit'
+}
+
+menuBurger.addEventListener('click', activeMenu)
+menuClose.addEventListener('click', closeMenu)
