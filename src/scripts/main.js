49634ef3ff7f90939cff4b54f031588ef139ad57
beliyandr <beliyandr@gmail.com>
2024-11-13 23:18:27 +0200

    swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,11 @@ const menuBurger = document.querySelector('.header__burder');
 const menuClose = document.querySelector('.menu__close');
 const body = document.querySelector('.wrapper');
 
+const form = document.querySelector('.subscribe__form');
+
 function activeMenu() {
   menu.classList.add('active');
-  body.style.overflow = 'hidden';
+  body.style.overflowY = 'hidden';
 
   menuLinks.forEach(item => {
     item.addEventListener('click', closeMenu)
@@ -16,8 +18,43 @@ function activeMenu() {
 }
 function closeMenu() {
   menu.classList.remove('active');
-  body.style.overflow = 'auto';
+  body.style.overflowY = 'auto';
+}
+
+function submitForm() {
+  const input = form.querySelector('input');
+  input.value = '';
 }
 
 menuBurger.addEventListener('click', activeMenu)
 menuClose.addEventListener('click', closeMenu)
+form.addEventListener('submit', submitForm)
+
+
+/* swiper-slider */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    720: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+
+
+  }
+})
