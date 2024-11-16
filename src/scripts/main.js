47f5fea627a8dd8a73a1c60af0fd17e4395fbc49
beliyandr <beliyandr@gmail.com>
2024-11-16 21:50:14 +0200

    slick

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,5 +28,54 @@ function submitForm() {
 
 menuBurger.addEventListener('click', activeMenu);
 menuClose.addEventListener('click', closeMenu);
-form.addEventListener('submit', submitForm);
+// form.addEventListener('submit', submitForm);
 
+
+/* --- */
+
+$('.slider').slick({
+  arrows: false,
+  dots: true,
+  adaptiveHeight: true,
+  slidesToShow: 2,
+  // slidesToScroll: 1,
+  // infinite: true,
+
+});
+
+
+
+// $('.responsive').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 2,
+//   slidesToScroll: 1,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         infinite: true,
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         infinite: true,
+//       }
+//     }
+//   ]
+// });
