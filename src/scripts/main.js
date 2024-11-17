e06fc95f5b2fb41b69c50cce671a21f8b3831999
beliyandr <beliyandr@gmail.com>
2024-11-17 21:59:52 +0200

    all

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,8 +28,6 @@ function submitForm() {
 
 menuBurger.addEventListener('click', activeMenu);
 menuClose.addEventListener('click', closeMenu);
-// form.addEventListener('submit', submitForm);
-
 
 /* --- */
 
@@ -38,44 +36,14 @@ $('.slider').slick({
   dots: true,
   adaptiveHeight: true,
   slidesToShow: 2,
-  // slidesToScroll: 1,
-  // infinite: true,
-
+  centerPadding: '20px',
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        slidesToShow: 1
+      }
+    },
+  ]
 });
-
-
-
-// $('.responsive').slick({
-//   dots: true,
-//   infinite: false,
-//   speed: 300,
-//   slidesToShow: 2,
-//   slidesToScroll: 1,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         infinite: true,
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         infinite: true,
-//       }
-//     }
-//   ]
-// });
