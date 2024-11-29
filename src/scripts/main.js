f57703844bfca0663b7a907c59c0ec577b4ef65a
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2024-11-30 00:01:48 +0200

    making header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const getBurger = document.querySelector('.icon--burger');
+const body = document.querySelector('.page__body');
+
+function getBurgerActive() {
+  getBurger.addEventListener('click', () => {
+    getBurger.classList.toggle('icon--active');
+    body.classList.toggle('page__body--lock');
+  })
+}
+
+getBurgerActive();
