e8a85d4626336f1c768503c66c404d77c8c847d8
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2024-12-09 18:29:30 +0200

    making menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const getBurger = document.querySelector('.icon--burger');
+const getMenu = document.querySelector('.page__menu');
 const body = document.querySelector('.page__body');
 
 function getBurgerActive() {
   getBurger.addEventListener('click', () => {
     getBurger.classList.toggle('icon--active');
     body.classList.toggle('page__body--lock');
+    getMenu.classList.toggle('page__menu--active');
   })
 }
 
