5b88c7adc174f655098aa6cd38d34a458a0753f3
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2024-12-12 23:38:44 +0200

    add fix menu bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,18 @@
 const getBurger = document.querySelector('.icon--burger');
 const getMenu = document.querySelector('.page__menu');
 const body = document.querySelector('.page__body');
+const navLinks = document.querySelectorAll('.nav__link')
+
+const massifButtons = [getBurger, ...navLinks];
 
 function getBurgerActive() {
-  getBurger.addEventListener('click', () => {
-    getBurger.classList.toggle('icon--active');
-    body.classList.toggle('page__body--lock');
-    getMenu.classList.toggle('page__menu--active');
-  })
+  for (let i = 0; i < massifButtons.length; i++) {
+    massifButtons[i].addEventListener('click', () => {
+      getBurger.classList.toggle('icon--active');
+      body.classList.toggle('page__body--lock');
+      getMenu.classList.toggle('page__menu--active');
+    })
+  }
 }
 
 getBurgerActive();
