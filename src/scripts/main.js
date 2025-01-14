64a5ff1bd10113144619993c61e3e53185b2e875
Jeny Berezhna <JenySmails24@gmail.com>
2025-01-14 19:06:28 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+/* document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("dropdown-menu");
+  const toggleButtons = document.querySelectorAll("#menu-toggle");
+
+  toggleButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      const isOpen = menu.classList.contains("is-active");
+      menu.classList.toggle("is-active", !isOpen);
+      menu.setAttribute("aria-hidden", isOpen);
+    });
+  });
+});
+ */
