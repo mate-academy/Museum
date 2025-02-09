f2ce885aaf32c567b1943bc57be3990b34afabb3
Andriian <an.hrytsan@gmail.com>
2025-02-09 18:55:46 +0200

    added first two sections

--- app.js
+++ app.js
@@ -17,12 +17,19 @@
 // });
 
 const dropdown = document.querySelector('.header__dropdown');
+// const dropdownVisibility = document.querySelector(
+//   '.header__dropdown--non-visible',
+// );
+const dropdownNavLinks = document.querySelectorAll(
+  '.header__dropdown-nav-link',
+);
 const openBtn = document.querySelector('.icon--dropdown-open');
 const closeBtn = document.querySelector('.icon--dropdown-close');
 const backdrop = document.getElementById('backdrop');
 
 // Відкриття дропдауну
 openBtn.addEventListener('click', () => {
+  dropdown.classList.remove('header__dropdown--non-visible');
   dropdown.classList.add('header__dropdown--active');
 
   // Чекаємо завершення анімації і тільки потім ховаємо/показуємо кнопки
@@ -46,5 +53,27 @@ closeBtn.addEventListener('click', () => {
     openBtn.style.opacity = '1'; // Відновлюємо прозорість для кнопки відкриття
     backdrop.classList.remove('backdrop--active'); // Ховаємо бекдроп
     closeBtn.classList.remove('fade-out'); // Прибираємо клас анімації
+    dropdown.classList.add('header__dropdown--non-visible');
   }, 200);
+});
+
+dropdownNavLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Запобігаємо миттєвому переходу
+
+    dropdown.classList.remove('header__dropdown--active');
+    backdrop.classList.remove('backdrop--active');
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'block';
+    openBtn.style.opacity = '1';
+
+    setTimeout(() => {
+      dropdown.classList.add('header__dropdown--non-visible');
+    }, 200);
+
+    // Затримуємо перехід на 150 мс
+    setTimeout(() => {
+      window.location.href = link.href; // Переходимо за посиланням
+    }, 220);
+  });
 });
