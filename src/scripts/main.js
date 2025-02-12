1157a83740e432ea6e680fce6f7284c8c5a539df
Artem Vasylenko <irbiskronos@gmail.com>
2025-02-12 18:20:52 +0200

    added gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 16,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1040: {
+      slidesPerView: 3,
+    },
+  },
+});
