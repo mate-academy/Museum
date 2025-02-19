bca6fece62e20ef0b6ad29b8299353063b1525c5
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-02-19 09:07:51 +0200

    add overlay and change scroll in aside

--- src/scripts/utils/burgerMenu.js
+++ src/scripts/utils/burgerMenu.js
@@ -3,12 +3,24 @@ export const burgerMenu = () => {
   const asideMenu = document.getElementById('aside');
   const closeMenu = document.getElementById('close-menu');
 
-  burgerMenu.addEventListener('click', () => {
-    asideMenu.classList.add('menu--active');
-  });
+  const overlay = document.createElement('div');
+  overlay.className = 'overlay';
+  document.body.appendChild(overlay);
 
-  closeMenu.addEventListener('click', (e) => {
+  const openMenu = () => {
+    asideMenu.classList.add('menu--active');
+    overlay.classList.add('overlay--active');
+    document.body.classList.add('no-scroll');
+  };
+
+  const closeMenuHandler = (e) => {
     e.preventDefault();
     asideMenu.classList.remove('menu--active');
-  });
+    overlay.classList.remove('overlay--active');
+    document.body.classList.remove('no-scroll');
+  };
+
+  burgerMenu.addEventListener('click', openMenu);
+  closeMenu.addEventListener('click', closeMenuHandler);
+  overlay.addEventListener('click', closeMenuHandler);
 };
