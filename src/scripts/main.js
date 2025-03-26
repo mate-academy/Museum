fbdffc015f1f660d495bb2043ff61a1bc58b69ec
Korshun <dmitriy.kor91@gmail.com>
2025-03-26 11:07:09 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      600: { slidesPerView: 2 },
+      850: { slidesPerView: 3 },
+    }
+  });
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const input = document.querySelector('.follow__input');
+
+  if (!input.value.trim()) {
+    alert('Please, write email!')
+    return false;
+  }
+
+  event.target.reset();
+
+  window.scrollTo(0, 0);
+
+  return false;
+}
