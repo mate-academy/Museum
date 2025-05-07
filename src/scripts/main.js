2ea88907faf57a44251ee017446a57f5212ca5ad
Kyrylo Golikov <pipecc1985@gmail.com>
2025-05-07 22:24:29 +0200

    need help with dots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,6 @@ const aside = document.querySelector('.aside');
 const menuON = document.querySelector('.header__menu');
 const menuOff = document.querySelector('.menu__button');
 const asideClose = document.querySelectorAll('.menu__galereyFoto__links');
-const slides = document.querySelectorAll('.gallery__slider-img');
-const dots = document.querySelectorAll('.gallery__dots__dot');
-let currentIndex = 0;
-let startX = 0;
 
 menuON.addEventListener('click', () => {
   aside.classList.add('aside__open');
@@ -24,30 +20,25 @@ asideClose.forEach(link => {
 });
 
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.toggle('gallery__slider-img--unactive', i !== index);
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.gallery__slider', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination.gallery__dots',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+    },
   });
-  dots.forEach((dot, i) => {
-    dot.classList.toggle('gallery__dots__dot--active', i === index);
-  });
-}
-
-document.querySelector('.gallery__slider').addEventListener('touchstart', (e) => {
-  startX = e.touches[0].clientX;
 });
-document.querySelector('.gallery__slider').addEventListener('touchend', (e) => {
-  const endX = e.changedTouches[0].clientX;
-  const diff = startX - endX;
 
-  if (Math.abs(diff) > 50) {
-    if (diff > 0 && currentIndex < slides.length - 1) {
-      currentIndex++; // свайп влево — следующий слайд
-    } else if (diff < 0 && currentIndex > 0) {
-      currentIndex--; // свайп вправо — предыдущий слайд
-    }
 
-    showSlide(currentIndex);
-  }
-});
+
 
