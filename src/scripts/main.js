8049c283e38e8dc16def7a6eb3398225f87eda7e
Daniel-spec-dell <fvgdanil@gmail.com>
2025-05-07 16:38:23 +0300

    add task for testing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const mySlider = new Splide("#mySlider", {
+  classes: {
+		pagination: 'splide__pagination gallery-section__pagination',
+		page      : 'splide__pagination__page gallery-section__pagination-page',
+  },
+
+
+  arrows: false,
+  pagination: true,
+});
+
+mySlider.mount();
+
+
+const submitButton = document.querySelector('#submitButton');
+const emailInput = document.querySelector('#emailInput');
+
+submitButton.addEventListener('click', function clean() {
+  emailInput.value = '';
+})
+
