fdb6786787970993917cc935c65d77a18bbfc8b9
Lirik <melnicukkiril2002@gmail.com>
2025-05-09 17:02:55 +0300

    Add carousel and gallery styles
    - Implemented styles for the carousel component, including layout, image handling, and pagination.
    - Created styles for the gallery component, enhancing grid layout and responsiveness across different devices.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      // loop: false,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+
+      spaceBetween: 30,
+    },
+  },
+});
