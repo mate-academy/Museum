23fa4424cb9c8ac18c4f2cfa3f3c903d5563aad7
Jeny Berezhna <JenySmails24@gmail.com>
2025-06-18 21:18:54 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,24 @@
 'use strict';
+/*
+const menuToggle = document.getElementById('menu-toggle');
+const closeButton = document.querySelector('.header__menu-icon-close');
+const menu = document.querySelector('.header__menu');
+const overlay = document.querySelector('.header__overlay');
+const root = document.body;
 
+function openMenu() {
+  root.classList.add('is-menu-open');
+  menu.setAttribute('aria-hidden', 'false');
+  overlay.classList.add('is-visible');
+}
 
-/* document.addEventListener("DOMContentLoaded", () => {
-  const menu = document.getElementById("dropdown-menu");
-  const toggleButtons = document.querySelectorAll("#menu-toggle");
+function closeMenu() {
+  root.classList.remove('is-menu-open');
+  menu.setAttribute('aria-hidden', 'true');
+  overlay.classList.remove('is-visible');
+}
 
-  toggleButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      const isOpen = menu.classList.contains("is-active");
-      menu.classList.toggle("is-active", !isOpen);
-      menu.setAttribute("aria-hidden", isOpen);
-    });
-  });
-});
- */
+menuToggle.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+*/
