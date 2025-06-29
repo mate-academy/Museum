6095063cbd12fb9802e46892a0530e9f27dae8ac
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-06-29 18:56:16 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.slider__nav-link');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active'));
+    button.classList.add('active');
+  })
+});
