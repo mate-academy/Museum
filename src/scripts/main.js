a53fa2953f6352e2a8006a5d833d6c44e33f856c
Roman <romoulichka20@gmail.com>
2025-06-29 20:30:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-'use strict';
+function initSlick() {
+  const $slider = $('.slider');
+  const windowWidth = $(window).width();
+
+
+  if (windowWidth < 1280) {
+    if (!$slider.hasClass('slick-initialized')) {
+      $slider.slick({
+        slidesToShow: windowWidth >= 768 ? 2 : 1,
+        slidesToScroll: 1,
+        centerMode: windowWidth >= 768 ? false : true,
+        centerPadding: '0px',
+        dots: true,
+        infinite: windowWidth >= 768 ? false : true,
+      });
+    }
+  } else {
+    if ($slider.hasClass('slick-initialized')) {
+      $slider.slick('unslick');
+    }
+  }
+}
+
+
+$(function () {
+  initSlick();
+});
+
+$(window).on('resize', function () {
+  initSlick();
+});
