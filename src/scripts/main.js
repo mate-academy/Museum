75157f741c2dcfd36497ff67d7c7c70513d5af99
Jeny Berezhna <JenySmails24@gmail.com>
2025-06-30 20:47:54 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,17 @@
 'use strict';
-/*
-const menuToggle = document.getElementById('menu-toggle');
-const closeButton = document.querySelector('.header__menu-icon-close');
-const menu = document.querySelector('.header__menu');
+
+const menuToggle = document.querySelector('.menu-toggle');
+const closeMenu = document.querySelector('.header__menu-icon-close');
 const overlay = document.querySelector('.header__overlay');
-const root = document.body;
 
 function openMenu() {
-  root.classList.add('is-menu-open');
-  menu.setAttribute('aria-hidden', 'false');
-  overlay.classList.add('is-visible');
+  document.body.classList.add('is-open');
 }
 
-function closeMenu() {
-  root.classList.remove('is-menu-open');
-  menu.setAttribute('aria-hidden', 'true');
-  overlay.classList.remove('is-visible');
+function closeMenuFn() {
+  document.body.classList.remove('is-open');
 }
 
 menuToggle.addEventListener('click', openMenu);
-closeButton.addEventListener('click', closeMenu);
-overlay.addEventListener('click', closeMenu);
-*/
+closeMenu.addEventListener('click', closeMenuFn);
+overlay.addEventListener('click', closeMenuFn);
